1e99f34eb8d9afc62f186c7f9128aeea119ead2c
Dean Allen <dean@textism.com>
2005-02-20 18:04:10 +0000

    Fix for silent windows when clicking Tags links in image tab
    git-svn-id: http://textpattern.googlecode.com/svn/current@68 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/include/txp_tag.php
+++ textpattern/include/txp_tag.php
@@ -682,6 +682,28 @@
 		return hed('Tag:',3).text_area('tag','100','300',$thing);
 	}
 
+// -------------------------------------------------------------
+	function tag_image() 
+	{
+		global $path_from_root,$img_dir;
+		$invars = gpsa(array('id','type','h','w','ext','alt'));
+		$img_dir = (!$img_dir) ? 'images' : $img_dir;
+		extract($invars);
+		switch ($type) {
+			case 'textile': 
+				$alt = ($alt) ? ' ('.$alt.')' : '';
+				$thing='!'.$path_from_root.$img_dir.'/'.$id.$ext.$alt.'!'; 
+			break;
+
+			case 'textpattern': $thing = '<txp:image id="'.$id.$ext.'" />'; break;
+
+			case 'xhtml': $thing = '<img src="'.$path_from_root.$img_dir.'/'.
+				$id.$ext.'" style="height:'.$h.'px;width:'.$w.'px" />';
+		}
+		return tdb($thing);
+	}
+
+
 ?>
 </body>
 </html>
