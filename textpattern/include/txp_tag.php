f05d87a97870001a02688e916ca5f542a1d3e74b
Sencer Yurdagül <alisencer@gmail.com>
2005-07-28 15:32:32 +0000

    Fix image path problems for tag-generator.
    git-svn-id: http://textpattern.googlecode.com/svn/current@649 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/include/txp_tag.php
+++ textpattern/include/txp_tag.php
@@ -711,22 +711,22 @@ $LastChangedRevision$
 // -------------------------------------------------------------
 	function tag_image() 
 	{
-		global $img_dir;
+		$img_dir = $GLOBALS['img_dir'];
 		$invars = gpsa(array('id','type','h','w','ext','alt'));
-		$img_dir = (rhu == '/') ? '' : '/';
+		$img_dir .= (rhu == '/') ? '' : '/';
 		$img_dir .= (!$img_dir) ? 'images' : $img_dir;
 		extract($invars);
 		switch ($type) {
 			case 'textile': 
 					$alt = ($alt) ? ' ('.$alt.')' : '';
-					$thing='!'.rhu.$img_dir.'/'.$id.$ext.$alt.'!'; 
+					$thing='!'.rhu.$img_dir.$id.$ext.$alt.'!'; 
 					break;
 			case 'textpattern': 
 					$thing = '<txp:image id="'.$id.$ext.'" />'; 
 					break;
 			case 'xhtml': 
 					$alt = ($alt) ? ' alt="'.$alt.'"' : '';
-					$thing = '<img src="'.rhu.$img_dir.'/'.
+					$thing = '<img src="'.rhu.$img_dir.
 							$id.$ext.'"'.$alt.' style="height:'.$h.'px;width:'.$w.'px" />';
 					break;
 		}
