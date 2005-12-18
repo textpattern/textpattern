6ae05e5ed128d0971db2b3369761c8c1692edddb
Sencer Yurdagül <alisencer@gmail.com>
2005-12-18 11:14:26 +0000

    fix discuss pageby (thx Andrew)
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@1152 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/include/txp_discuss.php
+++ textpattern/include/txp_discuss.php
@@ -17,7 +17,7 @@ $LastChangedRevision$
 	if ($event == 'discuss') {
 		require_privs('discuss');
 
-		if(!$step or !in_array($step, array('discuss_delete','discuss_save','discuss_list','discuss_edit','ipban_add','discuss_multi_edit','ipban_list','ipban_unban'))){
+		if(!$step or !in_array($step, array('discuss_delete','discuss_save','discuss_list','discuss_edit','ipban_add','discuss_multi_edit','ipban_list','ipban_unban','discuss_change_pageby'))){
 			discuss_list();
 		} else $step();
 	}
