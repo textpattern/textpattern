62e4229a0b6ce36609f79630060cc905f20a6ecf
Sencer Yurdagül <alisencer@gmail.com>
2005-12-29 00:22:58 +0000

    minor fix
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@1186 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/publish.php
+++ textpattern/publish.php
@@ -221,7 +221,7 @@ $LastChangedRevision$
 			$out['request_uri'] = @substr($argv[0], strpos($argv[0], ';' + 1));
 			// IIS again - given that the above doesn't always seem to help...
 		if (!$out['request_uri'] and serverSet('SCRIPT_NAME'))
-			$out['request_uri'] = serverSet('SCRIPT_NAME').( (serverSet('REQUEST_URI')) ? '?'.serverSet('REQUEST_URI') : '');
+			$out['request_uri'] = serverSet('SCRIPT_NAME').( (serverSet('QUERY_STRING')) ? '?'.serverSet('QUERY_STRING') : '');
 
 			// define the useable url, minus any subdirectories.
 			// this is pretty fugly, if anyone wants to have a go at it - dean
