textpattern/textpattern

commit 00a3ed3fca1d756b9491c8cc576d109fe4587f5d

populate thisarticle at page load on article pages; defer excerpt/body parse
git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@1303 2fea6a4d-a838-0410-917b-93a53c48e9d2

textpattern/publish.php
@@ -396,14 +396,14 @@ $LastChangedRevision$
 // 		$out['css']  = @$rs['css'];
 
 		if(is_numeric($id)) {
-			$idrs = safe_row("Posted, AuthorID, Keywords","textpattern","ID=".doSlash($id));
-			extract($idrs);
+			$a = safe_row('*, unix_timestamp(Posted) as uPosted', 'textpattern', "ID='".doSlash($id)."' and Status in (4,5)");
+			$Posted             = @$a['Posted'];
+			$out['id_keywords'] = @$a['AuthorID'];
+			$out['id_author']   = @$a['Keywords'];
+			populateArticleData($a);
 
 			if ($np = getNextPrev($id, $Posted, $s))
 				$out = array_merge($out, $np);
-
-			$out['id_keywords'] = $Keywords; 
-			$out['id_author']   = get_author_name($AuthorID); 
 		}
 
 		$out['path_from_root'] = $path_from_root; // these are deprecated as of 1.0
@@ -477,7 +477,12 @@ $LastChangedRevision$
 
 // -------------------------------------------------------------
 	function article($atts)
-	{		
+	{
+		global $is_article_body;
+		if ($is_article_body) {
+			trigger_error(gTxt('article_tag_illegal_body'));
+			return '';
+		}
 		return parseArticles($atts);
 	}
 
@@ -690,7 +695,7 @@ $LastChangedRevision$
 // -------------------------------------------------------------
 	function doArticle($atts) 
 	{
-		global $pretext,$prefs;
+		global $pretext,$prefs, $thisarticle;
 		extract($prefs);
 		extract($pretext);
 
@@ -702,18 +707,23 @@ $LastChangedRevision$
 			'status' => '',
 		),$atts));		
 
-		if ($status and !is_numeric($status))
-			$status = getStatusNum($status);
+		if ($status or empty($thisarticle) or $thisarticle['thisid'] != $id) {
+			if ($status and !is_numeric($status))
+				$status = getStatusNum($status);
 
-		$q_status = ($status ? "and Status='".doSlash($status)."'" : 'and Status in (4,5)');
+			$q_status = ($status ? "and Status='".doSlash($status)."'" : 'and Status in (4,5)');
 
-		$rs = safe_row("*, unix_timestamp(Posted) as uPosted", 
-				"textpattern", "ID='".intval($id)."' $q_status limit 1");
+			$rs = safe_row("*, unix_timestamp(Posted) as uPosted", 
+					"textpattern", "ID='".intval($id)."' $q_status limit 1");
 
-		if ($rs) {
-			extract($rs);
-			populateArticleData($rs);
-			global $thisarticle;
+			if ($rs) {
+				extract($rs);
+				populateArticleData($rs);
+			}
+		}
+
+		if (!empty($thisarticle)) {
+			extract($thisarticle);
 			$thisarticle['is_first'] = 1;
 			$thisarticle['is_last'] = 1;
 
@@ -750,7 +760,9 @@ $LastChangedRevision$
 		global $pretext, $is_article_list;
 		$old_ial = $is_article_list;
 		$is_article_list = ($pretext['id'] && !$iscustom)? false : true;
+		article_push();
 		$r = ($is_article_list)? doArticles($atts, $iscustom) : doArticle($atts);
+		article_pop();
 		$is_article_list = $old_ial;
 
 		return $r;
@@ -778,6 +790,8 @@ $LastChangedRevision$
 		$out['keywords']        = $Keywords;
 		$out['article_image']   = $Image;
 		$out['comments_count']  = $comments_count;
+		$out['body']            = $Body_html;
+		$out['excerpt']         = $Excerpt_html;
 
 
 		$custom = getCustomFields();
@@ -785,14 +799,9 @@ $LastChangedRevision$
 			foreach ($custom as $i => $name)
 				$out[$name] = $rs['custom_' . $i];
 		}
-			
-		global $thisarticle, $is_article_body;
-		$thisarticle = $out;
-		$is_article_body = 1;		
-		$thisarticle['body'] = parse($Body_html);
-		$thisarticle['excerpt'] = parse($Excerpt_html);
-		$is_article_body = 0;
-
+		
+		global $thisarticle;
+		$thisarticle = $out;	
 	}
 
 // -------------------------------------------------------------

textpattern/lib/txplib_misc.php
@@ -1481,8 +1481,19 @@ eod;
 			 @$txptrace[] = str_repeat("\t", @$txptracelevel).$msg;
 	}
 
+//-------------------------------------------------------------
+	function article_push() {
+		global $thisarticle, $stack_article;
+		$stack_article[] = @$thisarticle;
+	}
 
+//-------------------------------------------------------------
+	function article_pop() {
+		global $thisarticle, $stack_article;
+		$thisarticle = array_pop($stack_article);
+	}
 // -------------------------------------------------------------
+
 	function relative_path($path, $pfx=NULL)
 	{
 		if ($pfx === NULL)

textpattern/publish/atom.php
@@ -116,14 +116,14 @@ $LastChangedRevision$
 					$e['category1'] = (trim($Category1) ? '<category term="'.htmlspecialchars($Category1).'" />' : '');
 					$e['category2'] = (trim($Category2) ? '<category term="'.htmlspecialchars($Category2).'" />' : '');
 
-					$Excerpt = fixup_for_feed($thisarticle['excerpt'], permlinkurl($a));
+					$Excerpt = fixup_for_feed(parse($thisarticle['excerpt']), permlinkurl($a));
 					if ($syndicate_body_or_excerpt == 0)
-						$Body = fixup_for_feed($thisarticle['body'], permlinkurl($a));
+						$Body = fixup_for_feed(parse($thisarticle['body']), permlinkurl($a));
 					else {
 						$Body = '';
 						// If there's no excerpt, use body as content instead of body as summary
 						if (!trim($Excerpt))
-							$Body = fixup_for_feed($thisarticle['body'], permlinkurl($a));
+							$Body = fixup_for_feed(parse($thisarticle['body']), permlinkurl($a));
 					}
 
 					if (trim($Body))
@@ -144,21 +144,22 @@ $LastChangedRevision$
 			$limit = ($limit) ? $limit : $rss_how_many;
 			$limit = min($limit,max(100,$rss_how_many));
 		
-			$rs = safe_rows_start("*", "txp_link", "$cfilter order by date desc limit $limit");
+			$rs = safe_rows_start("*", "txp_link", "$cfilter order by date desc, id desc limit $limit");
 
 			if ($rs) {
 				while ($a = nextRow($rs)) {
 					extract($a);
  
-					$e['title'] = tag(doSpecial($linkname),'title');
-					$content = utf8_encode(htmlspecialchars($description));
-					$e['content'] = tag(n.$description.n,'content',t_texthtml);
-					
+					$e['title'] = tag(htmlspecialchars($linkname),'title',t_html);
+					$e['content'] = tag(n.htmlspecialchars($description).n,'content',t_html);
+
 					$url = (preg_replace("/^\/(.*)/","http://$siteurl/$1",$url));
 					$url = preg_replace("/&((?U).*)=/","&amp;\\1=",$url);
 					$e['link'] = '<link'.r_relalt.t_texthtml.' href="'.$url.'" />';
 
-					$e['issued'] = tag(gmdate('Y-m-d\TH:i:s\Z',strtotime($date)),'published');
+					$e['thisauthor'] = tag(n.t.t.t.tag(htmlspecialchars($thisauthor),'name').n.t.t,'author');
+
+					$e['issued'] = tag(safe_strftime('w3cdtf', strtotime($date)),'published');
 					$e['modified'] = tag(gmdate('Y-m-d\TH:i:s\Z',strtotime($date)),'updated');
 					$e['id'] = tag('tag:'.$mail_or_domain.','.$feed_time.':'.$id,'id');
 

textpattern/publish/rss.php
@@ -59,30 +59,23 @@ $LastChangedRevision$
 
 					$Body = (!$syndicate_body_or_excerpt) ? $thisarticle['body'] : $thisarticle['excerpt'];
 					$Body = (!trim($Body)) ? $thisarticle['body'] : $Body;
-					$Body = str_replace('href="/','href="'.hu,$Body);
-					$Body = preg_replace("/href=\\\"#(.*)\"/","href=\"".permlinkurl($a)."#\\1\"",$Body);
-					$Body = rss_safe_hed($Body);
-
-					$Body = preg_replace(array('/</','/>/',"/'/",'/"/'), array('&lt;','&gt;','&#039;','&quot;'),$Body);
-						// encode bare ampersands
-					$Body = preg_replace("/&(?![#0-9]+;|\w+;)/i",'&amp;', $Body);
+					$Body = escape_output(replace_relative_urls(parse($Body)));
 
 					$uTitle = ($url_title) ? $url_title : stripSpace($Title);
 					$uTitle = htmlspecialchars($uTitle,ENT_NOQUOTES);
-
 
 					if ($show_comment_count_in_feed) {
 						$count = ($comments_count > 0) ? ' ['.$comments_count.']' : '';
 					} else $count = '';
 
-					$Title = doSpecial($Title).$count;
+					$Title = escape_output(strip_tags($Title)).$count;
 
 					$permlink = permlinkurl($a);
 
-					$item = tag(strip_tags($Title),'title').n.
+					$item = tag($Title,'title').n.
 						tag($Body,'description').n.
 						tag($permlink,'link');
-	
+
 					$articles[$ID] = tag($item,'item');
 
 					$etags[$ID] = strtoupper(dechex(crc32($articles[$ID])));
@@ -92,13 +85,13 @@ $LastChangedRevision$
 
 			}
 		} elseif ($area=='link') {
-				
+
 			$cfilter = ($category) ? "category='$category'" : '1';
 			$limit = ($limit) ? $limit : $rss_how_many;
 			$limit = min($limit,max(100,$rss_how_many));
 
 			$rs = safe_rows_start("*", "txp_link", "$cfilter order by date desc limit $limit");
-		
+
 			if ($rs) {
 				while ($a = nextRow($rs)) {
 					extract($a);

textpattern/publish/taghandlers.php
@@ -1161,11 +1161,13 @@ $LastChangedRevision$
 
 function body($atts) 
 	{
-		global $thisarticle;
-
+		global $thisarticle, $is_article_body;
 		assert_article();
 		
-		return $thisarticle['body'];
+		$is_article_body = 1;		
+		$out = parse($thisarticle['body']);
+		$is_article_body = 0;
+		return $out;
 	}	
 	
 // -------------------------------------------------------------
@@ -1180,10 +1182,13 @@ function body($atts)
 // -------------------------------------------------------------
 	function excerpt($atts) 
 	{
-		global $thisarticle;
+		global $thisarticle, $is_article_body;
 		assert_article();
-
-		return $thisarticle['excerpt'];	
+		
+		$is_article_body = 1;		
+		$out = parse($thisarticle['excerpt']);
+		$is_article_body = 0;
+		return $out;
 	}
 
 // -------------------------------------------------------------

commit 32b8475674cf8952c64c827897edd17cd8709620

Fix override form bug introduced in r1303.
git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@1362 2fea6a4d-a838-0410-917b-93a53c48e9d2

textpattern/publish.php
@@ -792,7 +792,7 @@ $LastChangedRevision$
 		$out['comments_count']  = $comments_count;
 		$out['body']            = $Body_html;
 		$out['excerpt']         = $Excerpt_html;
-
+		$out['override_form']		= $override_form;
 
 		$custom = getCustomFields();
 		if ($custom) {