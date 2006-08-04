d6ddad9c2cc7815a020118f170d7b032e10f726a
Alex Shiels <alex@thresholdstate.com>
2006-08-04 05:53:23 +0000

    hide missing_article_tag error in Live mode
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@1617 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/publish.php
+++ textpattern/publish.php
@@ -451,7 +451,7 @@ $LastChangedRevision$
 		$html = ($prefs['allow_page_php_scripting']) ? evalString($html) : $html;
 
 		// make sure the page has an article tag if necessary
-		if (!$has_article_tag and (!empty($pretext['id']) or !empty($pretext['c']) or !empty($pretext['q']) or !empty($pretext['pg'])))
+		if (!$has_article_tag and $production_status != 'live' and (!empty($pretext['id']) or !empty($pretext['c']) or !empty($pretext['q']) or !empty($pretext['pg'])))
 			trigger_error(gTxt('missing_article_tag', array('{page}' => $pretext['page'])));
 
 		header("Content-type: text/html; charset=utf-8");
