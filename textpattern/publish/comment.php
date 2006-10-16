8568d5c256d399b2c68060ce40cfa94710a47fe7
Alex Shiels <alex@thresholdstate.com>
2006-10-16 21:03:32 +0000

    remove bogus space in comment input form
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@1950 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/publish/comment.php
+++ textpattern/publish/comment.php
@@ -181,7 +181,7 @@ $LastChangedRevision$
 
 		$vals = array(
 			'comment_name_input'		=> fInput('text', 'name', htmlspecialchars($name), 'comment_name_input'.($namewarn ? ' comments_error' : ''), '', '', $isize),
-			'comment_email_input'		=> fInput('text', 'email', htmlspecialchars($email), ' comment_email_input'.($emailwarn ? ' comments_error' : ''), '', '', $isize),
+			'comment_email_input'		=> fInput('text', 'email', htmlspecialchars($email), 'comment_email_input'.($emailwarn ? ' comments_error' : ''), '', '', $isize),
 			'comment_web_input'			=> fInput('text', 'web', htmlspecialchars($web)	, 'comment_web_input', '', '', $isize),
 			'comment_message_input' => $textarea.'<!-- plugin-place-holder -->',
 			'comment_remember'			=> $checkbox,
