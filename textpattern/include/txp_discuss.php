88778298fe38215111fa678095c10f941f8e4c69
Mary Fredborg <hi.mary@gmail.com>
2006-11-02 20:49:19 +0000

    Comments tab: allow for sorting comments by status.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@2061 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/include/txp_discuss.php
+++ textpattern/include/txp_discuss.php
@@ -106,6 +106,10 @@ $LastChangedRevision$
 				$sort_sql = 'message '.$dir.', posted asc';
 			break;
 
+			case 'status':
+				$sort_sql = "visible $dir, posted asc";
+			break;
+
 			case 'parent':
 				$sort_sql = 'parentid '.$dir.', posted asc';
 			break;
