a1e3abd09e3d0dbffa576ce074a27c9109644cbe
Alex Shiels <alex@thresholdstate.com>
2006-11-17 22:38:55 +0000

    fix updates for svn
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@2087 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/update/_update.php
+++ textpattern/update/_update.php
@@ -63,6 +63,12 @@ $LastChangedRevision$
 			$dbversion = '4.0.4';
 	}
 
+	if ( $dbversion !== '4.0.5' )
+	{  
+		if ((include txpath.DS.'update'.DS.'_to_4.0.5.php') !== false)
+			$dbversion = '4.0.5';
+	}
+
 	// keep track of updates for svn users
 	safe_delete('txp_prefs',"name = 'dbupdatetime'");
 	safe_insert('txp_prefs', "prefs_id=1, name='dbupdatetime',val='".max(newest_file(),time())."', type='2'");
