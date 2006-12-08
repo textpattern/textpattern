textpattern/textpattern

commit 2a03234106a8baf33b534b376cba1896190691b8

_Really_ fetch default css on txp_die();
git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@1659 2fea6a4d-a838-0410-917b-93a53c48e9d2

textpattern/publish/taghandlers.php
@@ -35,7 +35,7 @@ $LastChangedRevision$
 
 	function css($atts)
 	{
-		global $s;
+		global $txp_error_code, $s;
 
 		extract(lAtts(array(
 			'format' => 'url',
@@ -45,7 +45,12 @@ $LastChangedRevision$
 			'title'  => '',
 		), $atts));
 
-		if ($n)
+		if ($txp_error_code == '404')
+		{
+			$url = hu.'textpattern/css.php?n=default';
+		}
+	
+		elseif ($n)
 		{
 			$url = hu.'textpattern/css.php?n='.$n;
 		}

commit 22650db3c159c325d10a71539eac2836d03838a2

Fix txp:css bug caused by r1659.
git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@2126 2fea6a4d-a838-0410-917b-93a53c48e9d2

textpattern/publish/taghandlers.php
@@ -45,41 +45,26 @@ $LastChangedRevision$
 
 // -------------------------------------------------------------
 
-	function css($atts)
-	{
-		global $txp_error_code, $s;
+	function css($atts) {
+		global $s;
 
 		extract(lAtts(array(
 			'format' => 'url',
-			'media'  => 'screen',
-			'n'      => '',
-			'rel'    => 'stylesheet',
-			'title'  => '',
+			'media'	 => 'screen',
+			'n'			 => '',
+			'rel'		 => 'stylesheet',
+			'title'	 => '',
 		), $atts));
 
-		if ($txp_error_code == '404')
-		{
-			$url = hu.'textpattern/css.php?n=default';
-		}
-	
-		elseif ($n)
-		{
+		if ($n) {
 			$url = hu.'textpattern/css.php?n='.$n;
-		}
-
-		elseif ($s)
-		{
-			$sn = safe_field('css','txp_section',"name='".doSlash($s)."'");
-			$url = hu.'textpattern/css.php?n='.$sn;
-		}
-
-		else
-		{
+		} elseif ($s) {
+			$url = hu.'textpattern/css.php?s='.$s;
+		} else {
 			$url = hu.'textpattern/css.php?n=default';
 		}
 
-		if ($format == 'link')
-		{
+		if ($format == 'link') {
 			return '<link rel="'.$rel.'" type="text/css"'.
 				($media ? ' media="'.$media.'"' : '').
 				($title ? ' title="'.$title.'"' : '').