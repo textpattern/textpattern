8a5d61180ceb3c15aa8ae3c9a9271555e25d9830
Sencer Yurdagül <alisencer@gmail.com>
2007-06-28 22:57:45 +0000

    Add comment status to comment-notification mail.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@2456 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/publish/comment.php
+++ textpattern/publish/comment.php
@@ -404,6 +404,7 @@ $LastChangedRevision$
 		var $status;
 		var $message;
 		var $txpspamtrace = array();
+		var $status_text = array();
 
 		function comment_evaluation() {
 			global $prefs;
@@ -412,6 +413,11 @@ $LastChangedRevision$
 								   MODERATE => array(),
 								   VISIBLE  => array(),
 								   RELOAD  => array()
+								);
+			$this->status_text = array(	SPAM => gTxt('spam'), 
+									MODERATE => gTxt('unmoderated'), 
+									VISIBLE  => gTxt('visible'), 
+									RELOAD  => gTxt('reload')
 								);
 			$this->message = $this->status;
 			$this -> txpspamtrace[] = "Comment on $parentid by $name (".safe_strftime($prefs['archive_dateformat'],time()).")";
@@ -434,13 +440,13 @@ $LastChangedRevision$
 			if (trim($msg)) $this->message[$type][] = $msg;
 		}
 
-		function get_result() {
+		function get_result($result_type='numeric') {
 			$result = array();
 			foreach ($this->status as $key => $value)
 				$result[$key] = array_sum($value)/max(1,count($value));
 			arsort($result, SORT_NUMERIC);
 			reset($result);
-			return key($result);
+			return (($result_type == 'numeric') ? key($result) : $this->status_text[key($result)]);
 		}
 		function get_result_message() {
 			return $this->message[$this->get_result()];
@@ -545,11 +551,14 @@ $LastChangedRevision$
 		extract($article);
 		extract(safe_row("RealName, email", "txp_users", "name = '".doSlash($AuthorID)."'"));
 
+		$evaluator =& get_comment_evaluator();
+	
 		$out = gTxt('greeting')." $RealName,\r\n\r\n";
 		$out .= str_replace('{title}',$Title,gTxt('comment_recorded'))."\r\n";
 		$out .= permlinkurl_id($parentid)."\r\n";
 		if (has_privs('discuss', $AuthorID))
 			$out .= hu.'textpattern/index.php?event=discuss&step=discuss_edit&discussid='.$discussid."\r\n";
+		$out .= gTxt('status').": ".$evaluator->get_result('text').'. '.$evaluator->get_result_message()."\r\n";
 		$out .= "\r\n";
 		$out .= gTxt('comment_name').": $cname\r\n";
 		$out .= gTxt('comment_email').": $cemail\r\n";
