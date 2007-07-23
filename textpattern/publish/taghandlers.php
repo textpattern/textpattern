9ac0ff2bf92d1bc643e0fb8c86cc8613c6293cf6
Robert Wetzlmayr <r.wetzlmayr@gmail.com>
2007-07-23 18:40:09 +0000

    fix bug in active_class attribute for section_list and category_list tags that prevented mixed case section/category from being marked as active. Hopefully works in all URL modes.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@2503 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/publish/taghandlers.php
+++ textpattern/publish/taghandlers.php
@@ -923,7 +923,7 @@ $LastChangedRevision$
 					$section = ($this_section) ? ( $s == 'default' ? '' : $s ) : $section;
 
 					$out[] = tag(str_replace('& ', '&#38; ', $title), 'a', 
-						( ($active_class and ($c == strtolower($name))) ? ' class="'.$active_class.'"' : '' ).
+						( ($active_class and (0 == strcasecmp($c, $name))) ? ' class="'.$active_class.'"' : '' ).
 						' href="'.pagelinkurl(array('s' => $section, 'c' => $name)).'"'
 					);
 				}
@@ -995,7 +995,7 @@ $LastChangedRevision$
 				$url = pagelinkurl(array('s' => $name));
 
 				$out[] = tag($title, 'a', 
-					( ($active_class and ($s == strtolower($name))) ? ' class="'.$active_class.'"' : '' ).
+					( ($active_class and (0 == strcasecmp($s, $name))) ? ' class="'.$active_class.'"' : '' ).
 					' href="'.$url.'"'
 				);
 			}
