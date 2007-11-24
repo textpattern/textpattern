8a9f5964eeb949f23298d6ba6bab7f64b7376f39
Mary Fredborg <hi.mary@gmail.com>
2007-11-24 05:35:37 +0000

    Whoops, try that again!
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@2712 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/lib/txplib_head.php
+++ textpattern/lib/txplib_head.php
@@ -25,7 +25,7 @@ $LastChangedRevision$
 
 	?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 			"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo LANG; ?>" lang="<?php echo gTxt('lang_dir'); ?>">
+	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo LANG; ?>" lang="<?php echo LANG; ?>" dir="<?php echo gTxt('lang_dir'); ?>">
 	<head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta name="robots" content="noindex, nofollow" />
