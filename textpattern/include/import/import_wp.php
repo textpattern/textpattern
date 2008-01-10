6e1e6e5545eb576e0789f00fb3e407189b7ec46d
Mary Fredborg <hi.mary@gmail.com>
2008-01-10 09:32:36 +0000

    WordPress importer now imports links, link categories and users.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@2766 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/include/import/import_wp.php
+++ textpattern/include/import/import_wp.php
@@ -4,9 +4,6 @@
 	{
 		global $txpcfg;
 
-		// Keep some response on some part
-		$results = array();
-
 		$b2link = mysql_connect($b2dbhost, $b2dblogin, $b2dbpass, true);
 
 		if (!$b2link)
@@ -16,105 +13,295 @@
 
 		mysql_select_db($b2db, $b2link);
 
-		$results[] = hed('Connected to WordPress database. Importing Data&#8230;', 1).
-			n.'<ul>'.n;
+
+		// Keep some response on some part
+		$results = array();
+		$errors = array();
+
+		$results[] = hed('Connected to WordPress database. Importing Data&#8230;', 1);
+
+
+		/*
+		export users
+		*/
+
+		$users = array();
+
+
+		$user_query = mysql_query("
+			select
+				ID as user_id,
+				user_login as name,
+				user_email as email,
+				display_name as RealName
+			from ".$wpdbprefix."users
+		", $b2link) or $errors[] = mysql_error();
+
+		while ($user = mysql_fetch_array($user_query))
+		{
+			$user_privs_query = mysql_query("
+				select
+					meta_value
+				from ".$wpdbprefix."usermeta
+				where user_id = ".$user['user_id']." and meta_key = 'capabilities'
+			", $b2link) or $errors[] = mysql_error();
+
+			$privs = unserialize(mysql_result($user_privs_query, 0));
+
+			foreach ($privs as $key => $val)
+			{
+				// convert the built-in WordPress roles
+				// to their Txp equivalent
+				switch ($key)
+				{
+					// publisher
+					case 'administrator':
+						$user['privs'] = 1;
+					break;
+
+					// managing editor
+					case 'editor':
+						$user['privs'] = 2;
+					break;
+
+					// staff writer
+					case 'author':
+						$user['privs'] = 4;
+					break;
+
+					// freelancer
+					case 'contributor':
+						$user['privs'] = 5;
+					break;
+
+					// none
+					case 'subscriber':
+					default:
+						$user['privs'] = 0;
+					break;
+				}
+			}
+
+			$users[] = $user;
+		}
+
+
+		/*
+		export article and link categories
+		*/
+
+		$categories = array();
+
+		$category_query = mysql_query("
+			select 
+				t.slug as name, 
+				t.name as title,
+				tt.taxonomy as type,
+				tt.parent as parent
+			from ".$wpdbprefix."terms as t inner join ".$wpdbprefix."term_taxonomy as tt
+				on(t.term_id = tt.term_id)
+			order by field(tt.taxonomy, 'category','post_tag','link_category'), tt.parent asc, t.name asc
+		", $b2link) or $errors[] = mysql_error();
+
+		while ($category = mysql_fetch_array($category_query))
+		{
+			if ($category['parent'] != 0)
+			{
+				$category_parent_query = mysql_query("
+					select 
+						slug as name
+					from ".$wpdbprefix."terms 
+					where term_id = '".doSlash($category['parent'])."'
+				", $b2link) or $errors[] = mysql_error();
+
+				while ($parent = mysql_fetch_array($category_parent_query))
+				{
+					$category['parent'] = $parent['name'];
+				}
+			}
+			
+			else
+			{
+				$category['parent'] = 'root';
+			}
+
+			switch ($category['type'])
+			{
+				case 'post_tag':
+				case 'category':
+					$category['type'] = 'article';
+				break;
+				
+				case 'link_category':
+					$category['type'] = 'link';
+				break;
+			}
+
+			$categories[] = $category;
+		}
+
 
 		/*
 		export articles
 		*/
 
-		$a = mysql_query("
+		$article_query = mysql_query("
 			select
-				".$wpdbprefix."posts.ID as ID,
-				".$wpdbprefix."posts.post_status as Status,
-				".$wpdbprefix."posts.post_date as Posted,
-				".$wpdbprefix."posts.post_modified as LastMod,
-				".$wpdbprefix."posts.post_title as Title,
-				".$wpdbprefix."posts.post_content as Body,
-				".$wpdbprefix."posts.comment_status as Annotate,
-				".$wpdbprefix."posts.comment_count as comments_count,
-				".$wpdbprefix."post_name as url_title,
-				".$wpdbprefix."users.user_login as AuthorID
-			from ".$wpdbprefix."posts left join ".$wpdbprefix."users 
-				on ".$wpdbprefix."users.ID = ".$wpdbprefix."posts.post_author
-		", $b2link) or $results[] = mysql_error();
+				p.ID as ID,
+				p.post_status as Status,
+				p.post_date as Posted,
+				p.post_modified as LastMod,
+				p.post_title as Title,
+				p.post_content as Body,
+				p.comment_status as Annotate,
+				p.comment_count as comments_count,
+				p.post_name as url_title,
+				u.user_login as AuthorID
+			from ".$wpdbprefix."posts as p left join ".$wpdbprefix."users as u
+				on u.ID = p.post_author
+			order by p.ID asc
+		", $b2link) or $errors[] = mysql_error();
 
-		while ($b = mysql_fetch_array($a))
+		while ($article = mysql_fetch_array($article_query))
 		{
-			// Clean ugly wp slashes before to continue
-			$b = undoSlash(undoSlash($b));
+			// convert WP article status to Txp equivalent
+			switch ($article['Status'])
+			{
+				case 'draft':
+					$article['Status'] = 1;
+				break;
+
+				// hidden
+				case 'private':
+					$article['Status'] = 2;
+				break;
+
+				case 'pending':
+					$article['Status'] = 3;
+				break;
+
+				// live
+				case 'publish':
+					$article['Status'] = 4;
+				break;
+
+				default:
+					$article['Status'] = $insert_with_status;
+				break;
+			}
+
+			// convert WP comment status to Txp equivalent
+			switch ($article['Annotate'])
+			{
+				// on
+				case 'open':
+					$article['Annotate'] = 1;
+				break;
+
+				// off
+				case 'closed':
+				case 'registered_only':
+					$article['Annotate'] = 0;
+				break;
+			}
 
 			// article commments
 			$comments = array();
 
-			$c = mysql_query("
+			$comment_query = mysql_query("
 				select
-					".$wpdbprefix."comments.comment_author_IP as ip,
-					".$wpdbprefix."comments.comment_author as name,
-					".$wpdbprefix."comments.comment_author_email as email,
-					".$wpdbprefix."comments.comment_author_url as web,
-					".$wpdbprefix."comments.comment_content as message,
-					".$wpdbprefix."comments.comment_date as posted
-				from ".$wpdbprefix."comments 
-				where comment_post_ID = '".$b['ID']."'
-			", $b2link) or $results[]= mysql_error();
+					comment_author_IP as ip,
+					comment_author as name,
+					comment_author_email as email,
+					comment_author_url as web,
+					comment_content as message,
+					comment_date as posted
+				from ".$wpdbprefix."comments
+				where comment_post_ID = '".$article['ID']."'
+				order by comment_ID asc
+			", $b2link) or $errors[]= mysql_error();
 
-			while ($d = mysql_fetch_assoc($c))
+			while ($comment = mysql_fetch_assoc($comment_query))
 			{
-				$d = undoSlash(undoSlash($d));
-				$comments[] = $d;
+				$comments[] = $comment;
 			}
 
-			$b['comments'] = $comments;
+			$article['comments'] = $comments;
+
 
 			// article categories
-			$categories = array();
+			$article_categories = array();
 
-			$e = mysql_query("
+			$article_category_query = mysql_query("
 				select 
-					".$wpdbprefix."terms.name as title, 
-					".$wpdbprefix."terms.slug as name
-				from ".$wpdbprefix."terms inner join ".$wpdbprefix."term_taxonomy
-					on(".$wpdbprefix."terms.term_id = ".$wpdbprefix."term_taxonomy.term_id) 
-				inner join ".$wpdbprefix."term_relationships
-					on(".$wpdbprefix."term_taxonomy.term_taxonomy_id = ".$wpdbprefix."term_relationships.term_taxonomy_id)
-				where ".$wpdbprefix."term_relationships.object_id = '".$b['ID']."' and ".$wpdbprefix."term_taxonomy.taxonomy in('post_tag', 'category') 
-				order by ".$wpdbprefix."term_relationships.object_id asc, ".$wpdbprefix."terms.name asc
+					t.name as title, 
+					t.slug as name
+				from ".$wpdbprefix."terms as t inner join ".$wpdbprefix."term_taxonomy as tt
+					on(t.term_id = tt.term_id) 
+				inner join ".$wpdbprefix."term_relationships as tr
+					on(tt.term_taxonomy_id = tr.term_taxonomy_id)
+				where tr.object_id = '".$article['ID']."' and tt.taxonomy in('post_tag', 'category') 
+				order by tr.object_id asc, t.name asc
 				limit 2;
-			", $b2link) or $results[]= mysql_error();
+			", $b2link) or $errors[] = mysql_error();
 
-			while ($f = mysql_fetch_array($e))
+			while ($category = mysql_fetch_array($article_category_query))
 			{
-				$categories[] = $f;
+				$article_categories[] = $category;
 			}
 
-			$b['Category1'] = !empty($categories[0]) ? $categories[0]['name'] : '';
-			$b['Category2'] = !empty($categories[1]) ? $categories[1]['name'] : '';
+			$article['Category1'] = !empty($article_categories[0]) ? $article_categories[0]['name'] : '';
+			$article['Category2'] = !empty($article_categories[1]) ? $article_categories[1]['name'] : '';
 
-			$articles[] = $b;
+
+			$articles[] = $article;
 		}
 
 
 		/*
-		export categories
+		export links
 		*/
 
-		$cats = array();
+		$links = array();
 
-		$a = mysql_query("
-			select 
-				".$wpdbprefix."terms.slug as name, 
-				".$wpdbprefix."terms.name as title
-			from ".$wpdbprefix."terms inner join ".$wpdbprefix."term_taxonomy 
-				on(".$wpdbprefix."terms.term_id = ".$wpdbprefix."term_taxonomy.term_id)
-			where ".$wpdbprefix."term_taxonomy.taxonomy in('post_tag', 'category') 
-			order by ".$wpdbprefix."terms.name asc
-		", $b2link) or $results[] = mysql_error();
+		$link_query = mysql_query("
+			select
+				link_id as id,
+				link_name as linkname,
+				link_description as description,
+				link_updated as date,
+				link_url as url
+			from ".$wpdbprefix."links
+			order by link_id asc
+		", $b2link) or $errors[] = mysql_error();
 
-		while ($b = mysql_fetch_array($a))
+		while ($link = mysql_fetch_array($link_query))
 		{
-			$cats[] = $b;
+			// link categories
+			$link_categories = array();
+
+			$link_category_query = mysql_query("
+				select 
+					t.name as title, 
+					t.slug as name
+				from ".$wpdbprefix."terms as t inner join ".$wpdbprefix."term_taxonomy as tt
+					on(t.term_id = tt.term_id) 
+				inner join ".$wpdbprefix."term_relationships as tr
+					on(tt.term_taxonomy_id = tr.term_taxonomy_id)
+				where tr.object_id = '".$link['id']."' and tt.taxonomy = 'link_category' 
+				order by tr.object_id asc, t.name asc
+			", $b2link) or $errors[] = mysql_error();
+
+			while ($category = mysql_fetch_array($link_category_query))
+			{
+				$link['category'] = $category['name'];
+			}
+
+
+			$links[] = $link;
 		}
+
+
 
 		mysql_close($b2link);
 
@@ -139,9 +326,85 @@
 
 		mysql_select_db($txpdb, $txplink);
 
-		include txpath.'/lib/classTextile.php';
 
-		$textile = new Textile;
+		/*
+		import users
+		*/
+
+		if ($users)
+		{
+			include_once txpath.'/lib/txplib_admin.php';
+
+			$results[] = hed('Imported Users:', 2).
+				n.graf('Because WordPress uses a different password mechanism than Textpattern, you will need to reset each user&#8217;s password from <a href="index.php?event=admin">the Users tab</a>.').
+				n.'<ul>';
+
+			foreach ($users as $user)
+			{
+				extract($user);
+
+				if (!safe_row('user_id', 'txp_users', "name = '".doSlash($name)."'"))
+				{
+					$pass = doSlash(generate_password(6));
+					$nonce = doSlash(md5(uniqid(mt_rand(), TRUE)));
+
+					$rs = mysql_query("
+						insert into ".safe_pfx('txp_users')." set
+							name     = '".doSlash($name)."',
+							pass     = '".doSlash($pass)."',
+							email    = '".doSlash($email)."',
+							RealName = '".doSlash($RealName)."',
+							privs    = ".$privs.",
+							nonce    = '".doSlash($nonce)."'
+					", $txplink) or $errors[] = mysql_error();
+
+					if (mysql_insert_id())
+					{
+						$results[] = '<li>'.$name.' ('.$RealName.')</li>';
+					}
+				}
+			}
+
+			$results[] = '</ul>';
+		}
+
+
+
+		/*
+		import categories
+		*/
+
+		if ($categories)
+		{
+			$results[] = hed('Imported Categories:', 2).n.'<ul>';
+
+			foreach ($categories as $category)
+			{
+				extract($category);
+
+				if (!safe_row('id', 'txp_category', "name = '".doSlash($name)."' and type = '".doSlash($type)."' and parent = '".doSlash($parent)."'"))
+				{
+					$rs = mysql_query("
+						insert into ".safe_pfx('txp_category')." set
+							name   = '".doSlash($name)."',
+							title  = '".doSlash($title)."',
+							type   = '".doSlash($type)."',
+							parent = '".doSlash($parent)."'
+					", $txplink) or $errors[] = mysql_error();
+
+					if (mysql_insert_id())
+					{
+						$results[] = '<li>'.$title.' ('.$type.')</li>';
+					}
+				}
+			}
+
+			rebuild_tree_full('article');
+			rebuild_tree_full('link');
+
+			$results[] = '</ul>';
+		}
+
 
 		/*
 		import articles
@@ -149,76 +412,43 @@
 
 		if ($articles)
 		{
-			foreach ($articles as $a)
+			$results[] = hed('Imported Articles and Comments:', 2).n.'<ul>';
+
+			include txpath.'/lib/classTextile.php';
+
+			$textile = new Textile;
+
+			foreach ($articles as $article)
 			{
-				// some of WP's status' are supported by Txp directly,
-				// so let's try and keep those
-				switch ($a['Status'])
-				{
-					case 'draft':
-						$a['Status'] = 1;
-					break;
-
-					// hidden
-					case 'private':
-						$a['Status'] = 2;
-					break;
-
-					case 'pending':
-						$a['Status'] = 3;
-					break;
-
-					// live
-					case 'publish':
-						$a['Status'] = 4;
-					break;
-
-					default:
-						$a['Status'] = $insert_with_status;
-					break;
-				}
-
-				switch ($a['Annotate'])
-				{
-					case 'open':
-						$a['Annotate'] = 1;
-					break;
-
-					case 'closed':
-					case 'registered_only':
-						$a['Annotate'] = 0;
-					break;
-				}
+				extract($article);
 
 				// Ugly, really ugly way to workaround the slashes WP gotcha
-				$a['Body'] = str_replace('<!--more-->', '', $a['Body']);
-				$a['Body_html'] = $textile->textileThis($a['Body']);
-
-				extract($a);
+				$Body = str_replace('<!--more-->', '', $Body);
+				$Body_html = $textile->textileThis($Body);
 
 				// can not use array slash due to way on which comments are selected
-				$q = mysql_query("
+				$rs = mysql_query("
 					insert into ".safe_pfx('textpattern')." set
-					Posted		     = '".addslashes($Posted)."',
-					LastMod		     = '".addslashes($LastMod)."',
-					Title			     = '".addslashes($textile->TextileThis($Title, 1))."',
-					url_title      = '".addslashes($url_title)."',
-					Body			     = '".addslashes($Body)."',
-					Body_html      = '".addslashes($Body_html)."',
-					AuthorID	     = '".addslashes($AuthorID)."',
-					Category1      = '".addslashes($Category1)."',
-					Category2      = '".addslashes($Category2)."',
-					Section		     = '$insert_into_section',
-					uid            = '".md5(uniqid(rand(), true))."',
-					feed_time      = '".substr($Posted, 0, 10)."',
-					Annotate       = '".addslashes($Annotate)."',
-					AnnotateInvite = '$default_comment_invite',
-					Status		     = '".addslashes($Status)."'
-				", $txplink) or $results[] = mysql_error();
+						Posted		     = '".doSlash($Posted)."',
+						LastMod		     = '".doSlash($LastMod)."',
+						Title			     = '".doSlash($textile->TextileThis($Title, 1))."',
+						url_title      = '".doSlash($url_title)."',
+						Body			     = '".doSlash($Body)."',
+						Body_html      = '".doSlash($Body_html)."',
+						AuthorID	     = '".doSlash($AuthorID)."',
+						Category1      = '".doSlash($Category1)."',
+						Category2      = '".doSlash($Category2)."',
+						Section		     = '$insert_into_section',
+						uid            = '".md5(uniqid(rand(), true))."',
+						feed_time      = '".substr($Posted, 0, 10)."',
+						Annotate       = '".doSlash($Annotate)."',
+						AnnotateInvite = '$default_comment_invite',
+						Status		     = '".doSlash($Status)."'
+				", $txplink) or $errors[] = mysql_error();
 
-				if ($insertID = mysql_insert_id())
+				if ((int)$insert_id = mysql_insert_id())
 				{
-					$results[] = '<li>Imported article, &#8220;'.$Title.'&#8221; (<strong>ID#'.$insertID.'</strong>).</li>';
+					$results[] = '<li>'.$Title.'</li>';
 
 					if (!empty($comments))
 					{
@@ -231,61 +461,82 @@
 							// The ugly workaroud again
 							$message = nl2br($message);
 
-							$r = mysql_query("
+							$rs = mysql_query("
 								insert into ".safe_pfx('txp_discuss')." set
-									parentid = '$insertID',
-									name     = '$name',
-									email    = '$email',
-									web      = '$web',
-									ip       = '$ip',
-									posted   = '$posted',
-									message  = '$message',
+									parentid = '$insert_id',
+									name     = '".doSlash($name)."',
+									email    = '".doSlash($email)."',
+									web      = '".doSlash($web)."',
+									ip       = '".doSlash($ip)."',
+									posted   = '".doSlash($posted)."',
+									message  = '".doSlash($message)."',
 									visible  = 1
 							", $txplink) or $results[] = mysql_error();
 
-							if ($commentID = mysql_insert_id())
+							if (mysql_insert_id())
 							{
 								$inserted_comments++;
 							}
 						}
 
-						$results[] = '<li>Imported '.$inserted_comments.' of '.$comments_count.' comment(s) for article <strong>ID#'.$insertID.'</strong>.</li>';
+						$results[] = '<li>- '.$inserted_comments.' of '.$comments_count.' comment(s)</li>';
 					}
 				}
 			}
+
+			$results[] = '</ul>';
 		}
+
 
 		/*
-		import categories
+		import links
 		*/
 
-		if ($cats)
+		if ($links)
 		{
-			foreach ($cats as $cat)
+			$results[] = hed('Imported Links:', 2).n.'<ul>';
+
+			foreach ($links as $link)
 			{
-				extract(array_slash($cat));
+				extract($link);
 
-				if (!safe_row('id', 'txp_category', "name = '$name'"))
+				$rs = mysql_query("
+					insert into ".safe_pfx('txp_link')." set
+						linkname    = '".doSlash($linkname)."',
+						description = '".doSlash($description)."',
+						category    = '".doSlash($category)."',
+						date        = '".doSlash($date)."',
+						url         = '".doSlash($url)."'
+				", $txplink) or $errors[] = mysql_error();
+
+				if (mysql_insert_id())
 				{
-					$q = mysql_query("
-						insert into ".safe_pfx('txp_category')." set
-							name   = '".addslashes($name)."',
-							title  = '".addslashes($title)."',
-							type   = 'article',
-							parent = 'root'
-					", $txplink) or $results[] = mysql_error($q);
-
-					if (mysql_insert_id())
-					{
-						$results[] = '<li>Imported category, &#8220;'.$title.'&#8221;.</li>';
-					}
+					$results[] = '<li>'.$linkname.'</li>';
 				}
 			}
 
-			rebuild_tree_full('article');
+			$results[] = '</ul>';
 		}
 
-		return join(n, $results).n.'</ul>';
+
+		/*
+		show any errors we encountered
+		*/
+
+		if ($errors)
+		{
+			$results[] = hed('Errors Encountered:', 2).n.'<ul>';
+
+			foreach ($errors as $error)
+			{
+				$results[] = '<li>'.$error.'</li>';
+			}
+
+			$results[] = '</ul>';
+		}
+
+
+		return join(n, $results);
 	}
 
 	function undoSlash($in)
