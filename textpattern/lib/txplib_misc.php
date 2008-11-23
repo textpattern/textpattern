49a628d5d9e29b52ae8b1d6ba191c77a0bd83df6
Robert Wetzlmayr <r.wetzlmayr@gmail.com>
2008-11-23 19:34:23 +0000

    sanitizeForFile(): dumb down filename first, then remove unwanted chars.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@3009 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/lib/txplib_misc.php
+++ textpattern/lib/txplib_misc.php
@@ -698,7 +698,7 @@ $LastChangedRevision$
 	function sanitizeForFile($text)
 	{
 		// Remove anything but alphanumeric, dot, dash and underscore
-		$text = preg_replace('/[^a-zA-Z0-9._-]/', '', $text);
+		$text = preg_replace('/[^a-zA-Z0-9._-]/', '', dumbDown($text));
 		// Remove duplicate dots and any leading or trailing dots
 		$text = preg_replace('/[.]{2,}/', '.', trim($text, '.'));
 		return $text;
