ae385b27376f5606999d24e31165f30e89b571b8
Robert Wetzlmayr <r.wetzlmayr@gmail.com>
2008-11-25 17:13:32 +0000

    Avoid notice for RSS feeds with zero articles.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@3019 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/publish/rss.php
+++ textpattern/publish/rss.php
@@ -225,7 +225,7 @@ $LastChangedRevision$
 		$out = array_merge($out, $articles);
 
 		header("Content-Type: application/rss+xml; charset=utf-8");
-		if ($etag) header('ETag: "'.$etag.'"');
+		if (isset($etag)) header('ETag: "'.$etag.'"');
 		return
 			'<?xml version="1.0" encoding="utf-8"?>'.n.
 			'<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom">'.n.
