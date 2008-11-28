b651b3929b401725cfd332b7b367193afc84561d
Robert Wetzlmayr <r.wetzlmayr@gmail.com>
2008-11-28 19:06:04 +0000

    ...
    git-svn-id: http://textpattern.googlecode.com/svn/releases/4.0.7/source@3025 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/index.php
+++ textpattern/index.php
@@ -20,8 +20,8 @@ $LastChangedRevision$
 	define("txpath", dirname(__FILE__));
 	define("txpinterface", "admin");
 
-	$thisversion = '4.0.6';
-	$txp_using_svn = true; // set false for releases
+	$thisversion = '4.0.7';
+	$txp_using_svn = false; // set false for releases
 
 	ob_start(NULL, 2048);
 	if (!@include './config.php') {
