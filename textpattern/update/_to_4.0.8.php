9bde1ab6b50337a270409f1d11f6996403402039
Robert Wetzlmayr <r.wetzlmayr@gmail.com>
2008-11-30 19:09:08 +0000

    SHOW INDEX accepts no WHERE clause in MySQL < 5.x.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@3036 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/update/_to_4.0.8.php
+++ textpattern/update/_to_4.0.8.php
@@ -10,9 +10,18 @@ $LastChangedRevision: 3014 $
 		exit("Nothing here. You can't access this file directly.");
 	}
 
-	if (!getRows('show index from `'.PFX.'txp_plugin` where `Key_name`=\'status_type_idx\'')) {
+	$has_idx = 0;
+	$rs = getRows('show index from `'.PFX.'txp_plugin`');
+	foreach ($rs as $row) {
+		if ($row['Key_name'] == 'status_type_idx')
+			$has_idx = 1;
+	}
+
+	if (!$has_idx) {
 		safe_query('alter ignore table `'.PFX.'txp_plugin` add index status_type_idx(`status`, `type`)');
 	}
+
+	
 
 ?>
 
