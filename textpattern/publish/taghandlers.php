72c5be22291a424c078cd33aadd2d33c4736f434
Robert Wetzlmayr <r.wetzlmayr@gmail.com>
2009-06-02 12:09:55 +0000

    txp:recent_comments: Prevent double escapement of ampersands in article titles from [2866]. Thanks, LetterHoofd.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.0@3206 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/publish/taghandlers.php
+++ textpattern/publish/taghandlers.php
@@ -681,7 +681,7 @@ $LastChangedRevision$
 				if (empty($form) && empty($thing))
 				{
 					$out[] = href(
-						htmlspecialchars($c['name']).' ('.htmlspecialchars($c['title']).')',
+						htmlspecialchars($c['name']).' ('.escape_title($c['title']).')',
 						permlinkurl($c).'#c'.$c['discussid']
 					);
 				}
