656b1b3d9b945991f2876915131d44d331ca3140
Robert Wetzlmayr <r.wetzlmayr@gmail.com>
2009-11-20 06:04:56 +0000

    Set svn:keywords
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@3292 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/update/_to_4.3.0.php
+++ textpattern/update/_to_4.3.0.php
@@ -1,8 +1,8 @@
 <?php
 
 /*
-$HeadURL: https://textpattern.googlecode.com/svn/development/4.x/textpattern/update/_to_4.2.0.php $
-$LastChangedRevision: 3233 $
+$HeadURL$
+$LastChangedRevision$
 */
 
 	if (!defined('TXP_UPDATE'))
