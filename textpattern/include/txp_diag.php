d9731706fe0fe66d7477fbec46cde0eda3f37479
Robert Wetzlmayr <r.wetzlmayr@gmail.com>
2010-09-25 14:13:16 +0000

    white bits.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@3405 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/include/txp_diag.php
+++ textpattern/include/txp_diag.php
@@ -83,7 +83,7 @@ $LastChangedRevision$
 		'/update/_to_4.0.8.php',
 		'/update/_to_4.2.0.php',
 		'/update/_to_4.3.0.php',
-	'/update/_update.php'
+		'/update/_update.php'
 	);
 
 	$files_rpc = array(
