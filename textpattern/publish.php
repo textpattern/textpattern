62d0cec35d89b64eeb5c2c0bbe4b4d273acfaeaa
Robert Wetzlmayr <r.wetzlmayr@gmail.com>
2011-03-22 08:56:32 +0000

    Mind your indentation, lad!
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@3486 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/publish.php
+++ textpattern/publish.php
@@ -144,44 +144,43 @@ $LastChangedRevision$
 	if (@$s == 'file_download') {
 		callback_event('file_download');
 		if (!isset($file_error)) {
-
 			$filename = sanitizeForFile($filename);
 			$fullpath = build_file_path($file_base_path,$filename);
 
-				if (is_file($fullpath)) {
+			if (is_file($fullpath)) {
 
-					// discard any error php messages
-					ob_clean();
-					$filesize = filesize($fullpath); $sent = 0;
-					header('Content-Description: File Download');
-					header('Content-Type: application/octet-stream');
-					header('Content-Disposition: attachment; filename="'.$filename.'"; size = "'.$filesize.'"');
-					// Fix for lame IE 6 pdf bug on servers configured to send cache headers
-					header('Cache-Control: private');
-					@ini_set("zlib.output_compression", "Off");
-					@set_time_limit(0);
-					@ignore_user_abort(true);
-					if ($file = fopen($fullpath, 'rb')) {
-						while(!feof($file) and (connection_status()==0)) {
-							echo fread($file, 1024*64); $sent+=(1024*64);
-							ob_flush();
-							flush();
-						}
-						fclose($file);
-						// record download
-						if ((connection_status()==0) and !connection_aborted() ) {
-							safe_update("txp_file", "downloads=downloads+1", 'id='.intval($id));
-							log_hit('200');
-						} else {
-							$pretext['request_uri'] .= ($sent >= $filesize)
-								? '#aborted'
-								: "#aborted-at-".floor($sent*100/$filesize)."%";
-							log_hit('200');
-						}
+				// discard any error php messages
+				ob_clean();
+				$filesize = filesize($fullpath); $sent = 0;
+				header('Content-Description: File Download');
+				header('Content-Type: application/octet-stream');
+				header('Content-Disposition: attachment; filename="'.$filename.'"; size = "'.$filesize.'"');
+				// Fix for lame IE 6 pdf bug on servers configured to send cache headers
+				header('Cache-Control: private');
+				@ini_set("zlib.output_compression", "Off");
+				@set_time_limit(0);
+				@ignore_user_abort(true);
+				if ($file = fopen($fullpath, 'rb')) {
+					while(!feof($file) and (connection_status()==0)) {
+						echo fread($file, 1024*64); $sent+=(1024*64);
+						ob_flush();
+						flush();
 					}
-				} else {
-					$file_error = 404;
+					fclose($file);
+					// record download
+					if ((connection_status()==0) and !connection_aborted() ) {
+						safe_update("txp_file", "downloads=downloads+1", 'id='.intval($id));
+						log_hit('200');
+					} else {
+						$pretext['request_uri'] .= ($sent >= $filesize)
+							? '#aborted'
+							: "#aborted-at-".floor($sent*100/$filesize)."%";
+						log_hit('200');
+					}
 				}
+			} else {
+				$file_error = 404;
+			}
 		}
 
 		// deal with error
