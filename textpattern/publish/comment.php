92daa82da2d399945c898c9fec9b322b8d4f8e6f
Robert Wetzlmayr <r.wetzlmayr@gmail.com>
2012-05-31 05:57:36 +0000

    Comment form: Do not use 'url' input for web b/c some browsers are too strict.
    Reverts parts of r3624.
    
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@3797 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/publish/comment.php
+++ textpattern/publish/comment.php
@@ -192,7 +192,7 @@ $LastChangedRevision$
 		$vals = array(
 			'comment_name_input'		=> fInput('text', 'name', htmlspecialchars($name), 'comment_name_input'.($namewarn ? ' comments_error' : ''), '', '', $isize, '', 'name', false, $h5 && $comments_require_name),
 			'comment_email_input'		=> fInput($h5 ? 'email' : 'text', 'email', htmlspecialchars($email), 'comment_email_input'.($emailwarn ? ' comments_error' : ''), '', '', $isize, '', 'email', false, $h5 && $comments_require_email),
-			'comment_web_input'			=> fInput($h5 ? 'url' : 'text', 'web', htmlspecialchars($web)	, 'comment_web_input', '', '', $isize, '', 'web', false, false),
+			'comment_web_input'			=> fInput($h5 ? 'text' /* TODO: type = 'url' once browsers are less strict */ : 'text', 'web', htmlspecialchars($web)	, 'comment_web_input', '', '', $isize, '', 'web', false, false),
 			'comment_message_input' 	=> $textarea.'<!-- plugin-place-holder -->',
 			'comment_remember'			=> $checkbox,
 			'comment_preview'			=> fInput('submit', 'preview', gTxt('preview'), 'button', '', '', '', '', 'txpCommentPreview', false),
