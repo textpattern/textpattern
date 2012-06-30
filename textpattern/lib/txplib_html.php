a693d1776ab0347626df3b736b163dbf06fed399
Robert Wetzlmayr <r.wetzlmayr@gmail.com>
2012-06-30 05:38:20 +0000

    Visual feedback while we wait for txpAsyncHref().
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@3876 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/lib/txplib_html.php
+++ textpattern/lib/txplib_html.php
@@ -1195,7 +1195,7 @@ EOF;
 		$class = "$step async";
 		$href = "?event=$event&amp;step=$step&amp;thing=$thing&amp;property=$property";
 		if (AJAXALLY_CHALLENGED) {
-			$href .= '&amp;_txp_token='.form_token();
+			$href .= '&amp;value='.txpspecialchars($item).'&amp;_txp_token='.form_token();
 		}
 		return href($item, $href, $atts." class=\"$class\"");
 	}
--- textpattern/textpattern.js
+++ textpattern/textpattern.js
@@ -814,12 +814,20 @@ jQuery.fn.txpAsyncHref = function(options) {
         try {
             event.preventDefault();
             var obj = $(this);
-            var value = obj.text();
+
+            // Show feedback while processing
+            obj.addClass('busy');
+            $('body').addClass('busy');
+
             sendAsyncEvent(
                 // query string contains request params
-                this.search.replace('?', '') + '&value=' + value,
+                this.search.replace('?', '') + '&value=' + obj.text(),
                 function(data, textStatus, jqXHR) {
                     obj.html(data);
+
+                    // remove feedback elements
+                    obj.removeClass('busy');
+                    $('body').removeClass('busy');
                     if (options.success) options.success(obj, event, data, textStatus, jqXHR);
                     textpattern.Relay.callback('txpAsyncHref.success', {'this': obj, 'event': event, 'data': data, 'textStatus': textStatus, 'jqXHR': jqXHR});
                 },
