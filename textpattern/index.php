66f9720af2805970b5df0add2c8b3c960ada3402
Robert Wetzlmayr <r.wetzlmayr@gmail.com>
2012-07-30 13:55:22 +0000

    Tagging 4.5.0-beta.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@4032 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/index.php
+++ textpattern/index.php
@@ -57,7 +57,7 @@ $LastChangedRevision$
 
 	define("txpinterface", "admin");
 
-	$thisversion = '4.4.1';
+	$thisversion = '4.5.0-beta';
 	$txp_using_svn = true; // set false for releases
 
 	ob_start(NULL, 2048);
