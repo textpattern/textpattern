b7b2985099592047423ac4721b0560d12405f9e8
Phil Wareham <phil@designhive.com>
2012-10-04 19:15:37 +0000

    For better accessibility, alert close link should have title attribute
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@4176 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/theme/hive/hive.php
+++ textpattern/theme/hive/hive.php
@@ -159,7 +159,7 @@ class hive_theme extends theme
 			$html = ''; // TODO: Say what?
 			$js = 'window.alert("'.escape_js(strip_tags($thing[0])).'")';
 		} else {
-			$html = '<span role="alert" class="messageflash '.$class.'">'.gTxt($thing[0]).' <a role="button" href="#close" class="close">&times;</a></span>';
+			$html = '<span role="alert" class="messageflash '.$class.'">'.gTxt($thing[0]).' <a role="button" href="#close" class="close" title="'.gTxt('close').'">&times;</a></span>';
 			// Try to inject $html into the message pane no matter when _announce()'s output is printed
 			$js = escape_js($html);
 			$js = <<< EOS
