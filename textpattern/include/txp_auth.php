317414220019188edce01060ad28f275c0c54228
Jukka Svahn <jukka.m.svahn@gmail.com>
2012-10-11 12:50:19 +0000

    Load client i18n on login pages.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@4207 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/include/txp_auth.php
+++ textpattern/include/txp_auth.php
@@ -94,6 +94,8 @@ function doAuth()
 
 	function doLoginForm($message)
 	{
+		global $textarray_script;
+
 		include txpath.'/lib/txplib_head.php';
 
 		pagetop(gTxt('login'), $message);
@@ -160,6 +162,7 @@ $(document).ready(
 );
 EOSCR
 		).
+		n.script_js('textpattern.textarray = '.json_encode($textarray_script)).
 		n.'</div><!-- /txp-body -->'.n.'</body>'.n.'</html>';
 
 		exit(0);
