b94ac7cc909c262c09ee2ce0d40b399f7a888d76
Jukka Svahn <jukka.m.svahn@gmail.com>
2012-10-12 13:23:45 +0000

    Wave of documentation.
    Adds phpDoc comments for global/public elements.
    No code changes.
    
    See issue #274
    
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@4243 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/config-dist.php
+++ textpattern/config-dist.php
@@ -1,54 +1,88 @@
 <?php
 
-	/**
-	 *  mysql database
-	 */
+/**
+ * Database credentials and site configuration.
+ *
+ * @package DB
+ */
+
+/**
+ * MySQL database.
+ *
+ * @global string $txpcfg['db']
+ */
+
 		$txpcfg['db'] = 'databasename';
 
-	/**
-	 *  database login name
-	 */
+/**
+ * Database login name.
+ *
+ * @global string $txpcfg['user']
+ */
+
 		$txpcfg['user'] = 'root';
 
-	/**
-	 *  database password
-	 */
+/**
+ * Database password.
+ *
+ * @global string $txpcfg['pass']
+ */
+
 		$txpcfg['pass'] = '';
 
-	/**
-	 *  database host
-	 */
+/**
+ * Database hostname.
+ *
+ * @global string $txpcfg['host']
+ */
 
 		$txpcfg['host'] = 'localhost';
 
-	/**
-	 *  table prefix (Use ONLY if you require multiple installs in one db)
-	 */
+/**
+ * Table prefix.
+ *
+ * Use only if you require multiple installs in one db.
+ *
+ * @global string $txpcfg['table_prefix']
+ */
 
 		$txpcfg['table_prefix'] = '';
 
-	/**
-	 *  full server path to textpattern dir (no slash at end)
-	 */
+/**
+ * Full server path to textpattern directory, no slash at the end.
+ *
+ * @global string $txpcfg['txpath']
+ */
 
 		$txpcfg['txpath'] = '/home/path/to/textpattern';
 
-	/**
-	 *  DB Connection Charset, only for MySQL4.1 and up. Must be equal to the Table-Charset.
-	 */
+/**
+ * Database connection charset.
+ *
+ * Only for MySQL 4.1 and up. Must be equal to the table-charset, e.g. latin1 or utf8.
+ *
+ * @global string $txpcfg['dbcharset']
+ */
 
 		$txpcfg['dbcharset'] = 'utf8';
 
-	/**
-	 *  optional: database client flags as needed (@see http://www.php.net/manual/function.mysql-connect.php)
-	 */
+/**
+ * Database client flags.
+ *
+ * These are optional. Use the database client flags as needed.
+ * Available flags include: MYSQL_CLIENT_SSL, MYSQL_CLIENT_COMPRESS,
+ * MYSQL_CLIENT_IGNORE_SPACE, MYSQL_CLIENT_INTERACTIVE
+ *
+ * @global int $txpcfg['client_flags']
+ * @link   http://www.php.net/manual/function.mysql-connect.php
+ */
 
 	#	$txpcfg['client_flags'] = MYSQL_CLIENT_SSL | MYSQL_CLIENT_COMPRESS;
 
-	/**
-	 *  optional, advanced: http address of the site serving images
-	 *  @see http://forum.textpattern.com/viewtopic.php?id=34493
-	 */
+/*
+ * optional, advanced: http address of the site serving images
+ * see http://forum.textpattern.com/viewtopic.php?id=34493
+ */
 
 	# define('ihu', 'http://static.example.com/');
 
