e22f1aa8b66ff49a96308af607b2e9cd894a64bd
Jukka Svahn <jukka.m.svahn@gmail.com>
2012-10-13 01:58:48 +0000

    Programming style and consistency.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@4261 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/publish/search.php
+++ textpattern/publish/search.php
@@ -100,9 +100,13 @@
 	function filterSearch()
 	{
 		$rs = safe_column("name", "txp_section", "searchable != '1'");
-		if ($rs) {
-			foreach($rs as $name) $filters[] = "and Section != '".doSlash($name)."'";
-			return join(' ',$filters);
+		if ($rs)
+		{
+			foreach ($rs as $name)
+			{
+				$filters[] = "and Section != '".doSlash($name)."'";
+			}
+			return join(' ', $filters);
 		}
 		return false;
 	}
