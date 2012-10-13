7abb752e6e80e52b4cbfc0217ab7daf0afda17f7
Jukka Svahn <jukka.m.svahn@gmail.com>
2012-10-13 02:39:08 +0000

    Programming style and consistency.
    Few LFs, spaces and brackets.
    
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@4265 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/publish/log.php
+++ textpattern/publish/log.php
@@ -26,17 +26,21 @@
  *
  * @param int $status HTTP status code
  * @example
- * log_hit(202);
+ * log_hit(200);
  */
 
 	function log_hit($status)
 	{
 		global $nolog, $logging;
 		callback_event('log_hit');
-		if(!isset($nolog) && $status != '404') {
-			if($logging == 'refer') {
+		if (!isset($nolog) && $status != 404)
+		{
+			if ($logging == 'refer')
+			{
 				logit('refer', $status);
-			} elseif ($logging == 'all') {
+			}
+			elseif ($logging == 'all')
+			{
 				logit('', $status);
 			}
 		}
@@ -58,22 +62,27 @@
 	function logit($r = '', $status = 200)
 	{
 		global $siteurl, $prefs, $pretext;
-		$mydomain = str_replace('www.','',preg_quote($siteurl,"/"));
+		$mydomain = str_replace('www.', '', preg_quote($siteurl,"/"));
 		$out['uri'] = @$pretext['request_uri'];
-		$out['ref'] = clean_url(str_replace("http://","",serverSet('HTTP_REFERER')));
+		$out['ref'] = clean_url(str_replace("http://", "", serverSet('HTTP_REFERER')));
 		$ip = remote_addr();
 		$host = $ip;
 
-		if (!empty($prefs['use_dns'])) {
+		if (!empty($prefs['use_dns']))
+		{
 			// A crude rDNS cache
-			if ($h = safe_field('host', 'txp_log', "ip='".doSlash($ip)."' limit 1")) {
+			if ($h = safe_field('host', 'txp_log', "ip='".doSlash($ip)."' limit 1"))
+			{
 				$host = $h;
 			}
-			else {
+			else
+			{
 				// Double-check the rDNS
 				$host = @gethostbyaddr($ip);
 				if ($host != $ip and @gethostbyname($host) != $ip)
+				{
 					$host = $ip;
+				}
 			}
 		}
 
@@ -81,11 +90,23 @@
 		$out['host'] = $host;
 		$out['status'] = $status;
 		$out['method'] = serverSet('REQUEST_METHOD');
-		if (preg_match("/^[^\.]*\.?$mydomain/i", $out['ref'])) $out['ref'] = "";
 
-		if ($r=='refer') {
-			if (trim($out['ref']) != "") { insert_logit($out); }
-		} else insert_logit($out);
+		if (preg_match("/^[^\.]*\.?$mydomain/i", $out['ref']))
+		{
+			$out['ref'] = "";
+		}
+
+		if ($r == 'refer')
+		{
+			if (trim($out['ref']))
+			{
+				insert_logit($out);
+			}
+		}
+		else
+		{
+			insert_logit($out);
+		}
 	}
 
 /**
@@ -99,7 +120,10 @@
 	{
 		$in = doSlash($in);
 		extract($in);
-		safe_insert("txp_log", "`time`=now(),page='$uri',ip='$ip',host='$host',refer='$ref',status='$status',method='$method'");
+		safe_insert(
+			"txp_log",
+			"`time` = now(), page = '$uri', ip='$ip', host='$host', refer='$ref', status='$status', method='$method'"
+		);
 	}
 
 ?>
