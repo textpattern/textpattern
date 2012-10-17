7620d4d67040ce8d96474c485ebc6df92b0dde16
Jukka Svahn <jukka.m.svahn@gmail.com>
2012-10-17 13:36:22 +0000

    Programming style and consistency.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@4357 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- css.php
+++ css.php
@@ -6,8 +6,10 @@
  * @since 4.2.0
  */
 
-if (@ini_get('register_globals')) {
-	if (isset($_REQUEST['GLOBALS']) || isset($_FILES['GLOBALS'])) {
+if (@ini_get('register_globals'))
+{
+	if (isset($_REQUEST['GLOBALS']) || isset($_FILES['GLOBALS']))
+	{
 		die('GLOBALS overwrite attempt detected. Please consider turning register_globals off.');
 	}
 
@@ -24,7 +26,8 @@ if (@ini_get('register_globals')) {
 	// As the deliberate awkwardly-named local variable $_txpfoo MUST NOT be unset to avoid notices further
 	// down, we must remove any potential identically-named global from the list of global names here.
 	unset($_txpg['_txpfoo']);
-	foreach ($_txpg as $_txpfoo => $value) {
+	foreach ($_txpg as $_txpfoo => $value)
+	{
 		if (!in_array($_txpfoo, array(
 			'GLOBALS',
 			'_SERVER',
@@ -35,7 +38,8 @@ if (@ini_get('register_globals')) {
 			'_SESSION',
 			'_REQUEST',
 			'_ENV',
-		))) {
+		)))
+		{
 			unset($GLOBALS[$_txpfoo], $$_txpfoo);
 		}
 	}
@@ -69,6 +73,6 @@ define("txpinterface", "css");
 include txpath.'/publish.php';
 $s = gps('s');
 $n = gps('n');
-output_css($s,$n);
+output_css($s, $n);
 
 ?>
