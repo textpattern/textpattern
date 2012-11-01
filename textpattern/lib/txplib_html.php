669d6db16ae9e3b9af04ee27b7ef12cfe58ef613
Jukka Svahn <jukka.m.svahn@gmail.com>
2012-11-01 15:58:10 +0000

    startSkelTable() was deprecated in 4.4.0.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@4524 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/lib/txplib_html.php
+++ textpattern/lib/txplib_html.php
@@ -484,8 +484,8 @@
 /**
  * Renders start of a layout &lt;table&gt; element.
  *
- * @return string HTML
- * @deprecated ?
+ * @return     string HTML
+ * @deprecated 4.4.0
  */
 
 	function startSkelTable()
