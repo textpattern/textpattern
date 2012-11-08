d35188a61d8a36004edab243e1f97c519940b6fe
Phil Wareham <phil@designhive.com>
2012-11-08 11:34:59 +0000

    Spaces
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@4606 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/theme/hive/js/scripts.js
+++ textpattern/theme/hive/js/scripts.js
@@ -2,14 +2,14 @@ jQuery(function () {
 
 
 	// To make select navigation menu work
-	$('.txp-nav-select select').change(function() {
+	$('.txp-nav-select select').change(function () {
 		window.location = $(this).find('option:selected').val();
 	});
 
 
 	// External links open new window (target="_blank" replacement)
-	$('[rel="external"]').click( function() {
-		window.open( $(this).attr('href') );
+	$('[rel="external"]').click(function () {
+		window.open($(this).attr('href'));
 		return false;
 	});
 
