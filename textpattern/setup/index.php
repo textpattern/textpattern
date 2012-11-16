4cb1cbb2cb94081376792ff057aa34dd6d4e6232
Phil Wareham <phil@designhive.com>
2012-11-16 00:36:36 +0000

    Missed a textarea height in previous commit
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@4736 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/setup/index.php
+++ textpattern/setup/index.php
@@ -636,7 +636,7 @@ eod;
 				strong(setup_gTxt('before_you_proceed')).' '.setup_gTxt('create_config', array('{txpath}' => txpspecialchars(txpath)))
 			).
 
-		'<textarea class="code" readonly="readonly" name="config" cols="'.INPUT_LARGE.'" rows="'.INPUT_MEDIUM.'">'.
+		'<textarea class="code" readonly="readonly" name="config" cols="'.INPUT_LARGE.'" rows="16">'.
 		makeConfig($carry).
 		'</textarea>'.
 		'<form action="'.$_SERVER['PHP_SELF'].'" method="post">'.
