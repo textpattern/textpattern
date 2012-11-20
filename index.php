7097497955b057fe5782db4e79487bdd0b8b3112
Phil Wareham <phil@designhive.com>
2012-11-20 16:43:35 +0000

    Code consistency
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@4783 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 
 	// Make sure we display all errors that occur during initialization
 	error_reporting(E_ALL | E_STRICT);
-	@ini_set("display_errors","1");
+	@ini_set("display_errors", "1");
 
-	if (@ini_get('register_globals')) {
-		if (isset($_REQUEST['GLOBALS']) || isset($_FILES['GLOBALS'])) {
+	if (@ini_get('register_globals'))
+	{
+		if (isset($_REQUEST['GLOBALS']) || isset($_FILES['GLOBALS']))
+		{
 			die('GLOBALS overwrite attempt detected. Please consider turning register_globals off.');
 		}
 
@@ -19,10 +21,11 @@
 			(array) $_FILES,
 			(array) $_SERVER);
 
-		// As the deliberately awkward-named local variable $_txpfoo MUST NOT be unset to avoid notices further down
-		// we must remove any potentially identical-named global from the list of global names here.
+		// As the deliberately awkward-named local variable $_txpfoo MUST NOT be unset to avoid notices further
+		// down, we must remove any potentially identical-named global from the list of global names here.
 		unset($_txpg['_txpfoo']);
-		foreach ($_txpg as $_txpfoo => $value) {
+		foreach ($_txpg as $_txpfoo => $value)
+		{
 			if (!in_array($_txpfoo, array(
 				'GLOBALS',
 				'_SERVER',
@@ -46,16 +49,16 @@
 		define("txpath", dirname(__FILE__).'/textpattern');
 	}
 
-	// save server path to site root
+	// Save server path to site root.
 	if (!isset($here))
 	{
 		$here = dirname(__FILE__);
 	}
 
-	// pull in config unless configuration data has already been provided (multi-headed use).
+	// Pull in config unless configuration data has already been provided (multi-headed use).
 	if (!isset($txpcfg['table_prefix']))
 	{
-		// Use buffering to ensure bogus whitespace in config.php is ignored
+		// Use buffering to ensure bogus whitespace in config.php is ignored.
 		ob_start(NULL, 2048);
 		include txpath.'/config.php';
 		ob_end_clean();
@@ -63,13 +66,14 @@
 
 	include txpath.'/lib/constants.php';
 	include txpath.'/lib/txplib_misc.php';
+
 	if (!isset($txpcfg['table_prefix']))
 	{
 		txp_status_header('503 Service Unavailable');
 		exit('config.php is missing or corrupt.  To install Textpattern, visit <a href="./textpattern/setup/">textpattern/setup/</a>');
 	}
 
-	// custom caches et cetera?
+	// Custom caches, et cetera?
 	if (isset($txpcfg['pre_publish_script']))
 	{
 		require $txpcfg['pre_publish_script'];
