a9ad50296ab7f0ce759ab48bafad591e7f371611
Jukka Svahn <jukka.m.svahn@gmail.com>
2012-12-13 00:21:01 +0000

    Introduces 'TextpatternTextile::textileRestricted()'.
    Parses Textile in restricted mode according to Textpattern's
    preferences.
    
    See issue #338
    
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@5021 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/lib/txplib_misc.php
+++ textpattern/lib/txplib_misc.php
@@ -4693,6 +4693,36 @@ class TextpatternTextile extends Textile
 
 		parent::__construct($doctype);
 	}
+
+	/**
+	 * Parses content in a restricted mode.
+	 *
+	 * @param  string|null $text      The input document in textile format
+	 * @param  bool|null   $lite      Optional flag to switch the parser into lite mode
+	 * @param  bool|null   $noimage   Optional flag controlling the conversion of images into HTML img tags
+	 * @param  string|null $rel       Relationship to apply to all generated links
+	 * @return string      The text from the input document
+	 */
+
+	public function textileRestricted($text, $lite = null, $noimage = null, $rel = null)
+	{
+		if ($lite === null)
+		{
+			$lite = get_pref('comments_use_fat_textile', 1);
+		}
+
+		if ($noimage === null)
+		{
+			$noimage = get_pref('comments_disallow_images', 1);
+		}
+
+		if ($rel === null)
+		{
+			$rel = get_pref('comment_nofollow', 'nofollow');
+		}
+
+		return parent::textileRestricted($text, $lite, $noimage, $rel);
+	}
 }
 
 /**
