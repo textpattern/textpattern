34018dfa8bea09892d4f0fb2f056fd8d32bb93fd
Jukka Svahn <jukka.m.svahn@gmail.com>
2012-12-14 11:26:01 +0000

    Amend header.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@5048 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/vendors/Textpattern/Textile/Parser.php
+++ textpattern/vendors/Textpattern/Textile/Parser.php
@@ -1,23 +1,24 @@
 <?php
 
 /*
- * Textpattern Content Management System - http://textpattern.com
+ * Textpattern Content Management System
+ * http://textpattern.com
+ *
  * Copyright (C) 2012 The Textpattern Development Team
  *
  * This file is part of Textpattern.
  *
- * This program is free software; you can redistribute it and/or
+ * Textpattern is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation, version 2.
  *
- * This program is distributed in the hope that it will be useful,
+ * Textpattern is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * along with Textpattern. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
