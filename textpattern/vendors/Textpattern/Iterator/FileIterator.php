38345a1c30be42429deb91b3d46e3e364b22ef6f
Jukka Svahn <jukka.m.svahn@gmail.com>
2013-08-17 01:49:39 +0000

    Introduce Textpattern_Iterator_FileIterator.
    This chunk reading iterator will act as the base for
    newer, memory efficient Textpack parser and
    language interface among other things.
    
    The class name and what current() returns may later on
    still change. If we ever get string object, current will return
    that. Name may change if the location changes, e.g. if
    we get stack of filesystem functions to core.
    
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@5524 2fea6a4d-a838-0410-917b-93a53c48e9d2

---
+++ textpattern/vendors/Textpattern/Iterator/FileIterator.php
@@ -0,0 +1,171 @@
+<?php
+
+/*
+ * Textpattern Content Management System
+ * http://textpattern.com
+ *
+ * Copyright (C) 2013 The Textpattern Development Team
+ *
+ * This file is part of Textpattern.
+ *
+ * Textpattern is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * Textpattern is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Textpattern. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * File iterator.
+ *
+ * @since   4.6.0
+ * @package Iterator
+ * @example
+ * $file = new Textpattern_Iterator_FileIterator('file.txt');
+ * foreach ($file as $key => $line)
+ * {
+ * 	echo $line;
+ * }
+ */
+
+class Textpattern_Iterator_FileIterator implements Iterator
+{
+	/**
+	 * Filename.
+	 *
+	 * @var string
+	 */
+
+	protected $filename;
+
+	/**
+	 * Line length.
+	 *
+	 * @var int
+	 */
+
+	protected $lineLength = 4096;
+
+	/**
+	 * Filepointer.
+	 *
+	 * @var resource
+	 */
+
+	protected $filepointer;
+
+	/**
+	 * The current element.
+	 */
+
+	protected $current;
+
+	/**
+	 * The current index.
+	 *
+	 * @var int
+	 */
+
+	protected $key = -1;
+
+	/**
+	 * Whether its valid or not.
+	 *
+	 * @var bool
+	 */
+
+	protected $valid;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $filename The filename
+	 * @param int    $length   Line length
+	 */
+
+	public function __construct($filename, $length = 4096)
+	{
+		$this->filename = $filename;
+		$this->lineLength = (int) $length;
+	}
+
+	/**
+	 * Destructor.
+	 */
+
+	public function __destruct()
+	{
+		if (is_resource($this->filepointer))
+		{
+			fclose($this->filepointer);
+		}
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+
+	public function current()
+	{
+		return $this->current;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+
+	public function key()
+	{
+		return $this->key;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+
+	public function next()
+	{
+		if (!feof($this->filepointer))
+		{
+			$this->current = fgets($this->filepointer, $this->lineLength);
+			$this->key++;
+			$this->valid = true;
+		}
+		else
+		{
+			$this->_valid = false;
+			fclose($this->filepointer);
+		}
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+
+	public function rewind()
+	{
+		if (is_resource($this->filepointer) === false)
+		{
+			$this->filepointer = fopen($this->filename, 'r');
+		}
+
+		rewind($this->filepointer);
+		$this->key = -1;
+		$this->next();
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+
+	public function valid()
+	{
+		return $this->valid;
+	}
+}
