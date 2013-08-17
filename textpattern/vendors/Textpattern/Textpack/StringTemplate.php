ef271216986b62460759e1c4033990e9d291b8ae
Jukka Svahn <jukka.m.svahn@gmail.com>
2013-08-17 22:16:26 +0000

    Interface for single Textpack string.
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@5529 2fea6a4d-a838-0410-917b-93a53c48e9d2

---
+++ textpattern/vendors/Textpattern/Textpack/StringTemplate.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ * Textpattern Content Management System
+ * http://textpattern.com
+ *
+ * Copyright (C) 2013 The Textpattern Development Team
+ *
+ * This file is part of Textpattern.
+ *
+ * Textpattern is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * Textpattern is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Textpattern. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Textpack string template.
+ *
+ * @since   4.6.0
+ * @package Textpack
+ */
+
+interface Textpattern_Textpack_StringTemplate
+{
+	/**
+	 * Gets the name of the string.
+	 *
+	 * @return string
+	 */
+
+	public function getName();
+
+	/**
+	 * Gets the language.
+	 *
+	 * @return string
+	 */
+
+	public function getLanguage();
+
+	/**
+	 * Gets the translation string contents.
+	 *
+	 * @return string
+	 */
+
+	public function getString();
+
+	/**
+	 * Gets the event.
+	 *
+	 * @return string
+	 */
+
+	public function getEvent();
+
+	/**
+	 * Gets the owner.
+	 *
+	 * @return string
+	 */
+
+	public function getOwner();
+
+	/**
+	 * Gets the version.
+	 *
+	 * @return string
+	 */
+
+	public function getVersion();
+
+	/**
+	 * Gets the last modification timestamp.
+	 *
+	 * @return int
+	 */
+
+	public function getLastmod();
+}
