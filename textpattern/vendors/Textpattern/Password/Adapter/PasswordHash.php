a5c1aa22dd75376f5221d4bdb43e60b5d87ed708
Jukka Svahn <jukka.m.svahn@gmail.com>
2013-08-18 22:28:17 +0000

    Introduces Textpattern_Password_Hash.
    Adaptable password hashing class. By default implements
    a PHPass as the hashing provider.
    
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@5534 2fea6a4d-a838-0410-917b-93a53c48e9d2

---
+++ textpattern/vendors/Textpattern/Password/Adapter/PasswordHash.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * Textpattern Content Management System
+ * http://textpattern.com
+ *
+ * Copyright (C) 2013 The Textpattern Development Team
+ *
+ * This file is part of Textpattern.
+ *
+ * Textpattern is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * Textpattern is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Textpattern. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Adapter for PHPass.
+ *
+ * @since   4.6.0
+ * @package Password
+ */
+
+class Textpattern_Password_Adapter_PasswordHash implements Textpattern_Password_ProviderInterface
+{
+	/**
+	 * Stores an instance of PHPass.
+	 *
+	 * @var PasswordHash
+	 */
+
+	private $phpass;
+
+	/**
+	 * Constructor.
+	 */
+
+	public function __construct()
+	{
+		$this->phpass = new PasswordHash(PASSWORD_COMPLEXITY, PASSWORD_PORTABILITY);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+
+	public function verify($password, $hash)
+	{
+		return $this->phpass->CheckPassword($password, $hash);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+
+	public function hash($password)
+	{
+		return $this->phpass->HashPassword($password);
+	}
+}
