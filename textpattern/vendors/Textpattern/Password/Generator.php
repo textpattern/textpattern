b949db14eea02c3b1cf3d7bfe6d49aeb4c539613
Jukka Svahn <jukka.m.svahn@gmail.com>
2013-08-19 13:58:52 +0000

    Introduces Textpattern_Password_Generator.
    The old generate_password() as an class.
    
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@5538 2fea6a4d-a838-0410-917b-93a53c48e9d2

---
+++ textpattern/vendors/Textpattern/Password/Generator.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+ * Textpattern Content Management System
+ * http://textpattern.com
+ *
+ * Copyright (C) 2013 The Textpattern Development Team
+ *
+ * This file is part of Textpattern.
+ *
+ * Textpattern is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * Textpattern is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Textpattern. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Generates a password.
+ *
+ * @since   4.6.0
+ * @package Password
+ * @example
+ * $password = new Textpattern_Password_Generator();
+ * echo $password->generate(16);
+ */
+
+class Textpattern_Password_Generator
+{
+	/**
+	 * Stores the character table.
+	 *
+	 * @var array
+	 */
+
+	protected $chars;
+
+	/**
+	 * Gets the character table.
+	 *
+	 * @return array
+	 */
+
+	public function getCharacterTable()
+	{
+		if (!$this->chars)
+		{
+			$this->chars = str_split(PASSWORD_SYMBOLS);
+		}
+
+		return $this->chars;
+	}
+
+	/**
+	 * Generates a random password.
+	 *
+	 * @param  int    $length The length of the generated password
+	 * @return string The password
+	 * @example
+	 * $password = new Textpattern_Password_Generator();
+	 * echo $password->generate(16);
+	 */
+
+	public function generate($length)
+	{
+		$pool = false;
+		$pass = '';
+
+		for ($i = 0; $i < $length; $i++)
+		{
+			if (!$pool)
+			{
+				$pool = $this->getCharacterTable();
+			}
+
+			$index = mt_rand(0, count($pool) - 1);
+			$pass .= $pool[$index];
+			unset($pool[$index]);
+			$pool = array_values($pool);
+		}
+
+		return $pass;
+	}
+}
