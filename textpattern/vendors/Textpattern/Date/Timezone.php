092ea1fe3368d18ad779695e33711e723f460e4c
Phil Wareham <phil@designhive.com>
2013-12-01 11:24:34 +0000

    Daylight

--- textpattern/vendors/Textpattern/Date/Timezone.php
+++ textpattern/vendors/Textpattern/Date/Timezone.php
@@ -209,7 +209,7 @@ class Textpattern_Date_Timezone
 	}
 
 	/**
-	 * Gets the next day light saving transition period for the given timezone.
+	 * Gets the next daylight saving transition period for the given timezone.
 	 *
 	 * Returns FALSE if the timezone does not use DST.
 	 *
