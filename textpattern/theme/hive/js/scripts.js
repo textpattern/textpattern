fb0ba2035d5c69b906a61cf3641570d6e54643c4
Phil Wareham <phil@designhive.com>
2014-05-04 07:54:03 +0000

    Various tab indents converted to spaces
    git-svn-id: http://textpattern.googlecode.com/svn/development/4.x@5838 2fea6a4d-a838-0410-917b-93a53c48e9d2

--- textpattern/theme/hive/js/scripts.js
+++ textpattern/theme/hive/js/scripts.js
@@ -1,15 +1,15 @@
 jQuery(function ()
 {
-	// To make select navigation menu work
-	$('.txp-nav-select select').change(function ()
-	{
-		window.location = $(this).find('option:selected').val();
-	});
+    // To make select navigation menu work
+    $('.txp-nav-select select').change(function ()
+    {
+        window.location = $(this).find('option:selected').val();
+    });
 
-	// External links open new window (target="_blank" replacement)
-	$('[rel="external"]').click(function ()
-	{
-		window.open($(this).attr('href'));
-		return false;
-	});
+    // External links open new window (target="_blank" replacement)
+    $('[rel="external"]').click(function ()
+    {
+        window.open($(this).attr('href'));
+        return false;
+    });
 });
