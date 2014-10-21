6a0212d3071974a352ad28f0e9dd9c95cdf947ac
Phil Wareham <phil@designhive.com>
2014-10-21 11:43:40 +0100

    Add `.prev-next` class to article tertiary navigation links
    Fixes #432. Thanks Philippe Wittenbergh.

--- textpattern/include/txp_article.php
+++ textpattern/include/txp_article.php
@@ -1785,7 +1785,7 @@ function article_partial_article_nav($rs)
         ));
     }
 
-    return graf(join('', $out), ' role="navigation" class="nav-tertiary"');
+    return graf(join('', $out), ' role="navigation" class="nav-tertiary prev-next"');
 }
 
 /**
