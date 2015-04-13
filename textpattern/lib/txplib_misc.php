74e348b26de81fb44ea7c61d8965867d73bdc0bb
Robert Wetzlmayr <r.wetzlmayr@gmail.com>
2015-04-13 15:59:18 +0200

    Do not remove commas in escape_js();
    Fixes #470.

--- textpattern/lib/txplib_misc.php
+++ textpattern/lib/txplib_misc.php
@@ -279,7 +279,7 @@ function escape_title($title)
 
 function escape_js($js)
 {
-    $js = preg_replace('/[\x{2028},\x{2029}]/u', '', $js);
+    $js = preg_replace('/[\x{2028}\x{2029}]/u', '', $js);
 
     return addcslashes($js, "\\\'\"\n\r");
 }
