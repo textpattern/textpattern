15ee48b8a8ad21715665be26c291d8e6770033d3
Stef Dawson <thatbloke@stefdawson.com>
2015-10-10 19:00:50 +0100

    Catch language switch error if locale missing
    See http://forum.textpattern.com/viewtopic.php?id=40685

--- textpattern/include/txp_lang.php
+++ textpattern/include/txp_lang.php
@@ -309,12 +309,18 @@ function save_language()
     )));
 
     if (safe_field('lang', 'txp_lang', "lang='".doSlash($language)."' limit 1")) {
-        $locale = $prefs['locale'] = Txp::get('Textpattern_L10n_Locale')->getLanguageLocale($language);
-        Txp::get('Textpattern_L10n_Locale')->setLocale(LC_ALL, $language);
-        set_pref('locale', $locale);
-        set_pref('language', $language);
-        $textarray = load_lang($language);
-        list_languages(gTxt('preferences_saved'));
+        try {
+            $locale = $prefs['locale'] = Txp::get('Textpattern_L10n_Locale')->getLanguageLocale($language);
+            Txp::get('Textpattern_L10n_Locale')->setLocale(LC_ALL, $language);
+            set_pref('locale', $locale);
+            set_pref('language', $language);
+            $textarray = load_lang($language);
+            $message = gTxt('language_changed');
+        } catch (Exception $e) {
+            $message = array(gTxt('language_not_changed', array('{name}' => $language)), E_ERROR);
+        }
+
+        list_languages($message);
 
         return;
     }
