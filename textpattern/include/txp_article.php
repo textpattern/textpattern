8f26da57fec4256686a28fe9948cda09f15793e6
Phil Wareham <phil@designhive.com>
2015-10-28 17:13:17 +0000

    Semantics

--- textpattern/include/txp_article.php
+++ textpattern/include/txp_article.php
@@ -1891,7 +1891,7 @@ function article_partial_article_nav($rs)
         ));
     }
 
-    return graf(join('', $out), ' class="nav-tertiary" role="navigation"');
+    return n.tag(join('', $out), 'nav', array('class' => 'nav-tertiary'));
 }
 
 /**
