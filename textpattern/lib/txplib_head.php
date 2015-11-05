ac9d06bd403123f76cad5c1161ba05b56e2f01e1
Phil Wareham <phil@designhive.com>
2015-11-05 09:17:58 +0000

    Constancy classname pattern

--- textpattern/lib/txplib_head.php
+++ textpattern/lib/txplib_head.php
@@ -70,10 +70,10 @@ function pagetop($pagetitle, $message = '')
 
     if (gps('logout')) {
         $body_id = 'page-logout';
-        $area = 'login-pane';
+        $area = 'login';
     } elseif (!$txp_user) {
         $body_id = 'page-login';
-        $area = 'login-pane';
+        $area = 'login';
     } else {
         $body_id = 'page-'.txpspecialchars($event);
     }
