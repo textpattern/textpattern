0a2df88899fe3f12dc80d28272cf6fb801030b13
Ruud van Melick <ruud@vanmelick.com>
2016-01-09 20:08:16 +0000

    No zero dates in SQL. Category name 64 chars. Re-create txp_token
    MySQL 5.7 by default no longer allows 0000-00-00 dates, which were used as the default on several columns in TXP tables.
    This patch removes those defaults for NOT NULL columns and sets the default to NULL for NULL columns (textpattern:Expires and txp_users:last_access)
    Zero dates in existing rows are converted to sensible values that are allowed.
    
    Categories, when created or edited can be 64 chars long. A few other tables that used these categories allowed for longer names. That doesn't make sense, because they can't be longer than 64 chars, so I've changed those colums to varchar(64).
    
    txp_token was added to the update scripts, but without NOT NULL and it was also missing from txpsql.php. Added it there and recreated the table to be more in line with the other tables.

--- textpattern/update/_to_4.0.5.php
+++ textpattern/update/_to_4.0.5.php
@@ -41,18 +41,19 @@ if (!in_array('status', $txpfile)) {
 $update_files = 0;
 
 if (!in_array('modified', $txpfile)) {
-    safe_alter('txp_file', "ADD modified DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00'");
+    safe_alter('txp_file', "ADD modified DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00'");
+    safe_alter('txp_file', "MODIFY modified DATETIME NOT NULL");
     $update_files = 1;
 }
 
 if (!in_array('created', $txpfile)) {
-    safe_alter('txp_file', "ADD created DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00'");
+    safe_alter('txp_file', "ADD created DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00'");
+    safe_alter('txp_file', "MODIFY created DATETIME NOT NULL");
     $update_files = 1;
 }
 
 if (!in_array('size', $txpfile)) {
-    safe_alter('txp_file',
-        "ADD size BIGINT");
+    safe_alter('txp_file', "ADD size BIGINT");
     $update_files = 1;
 }
 
@@ -61,9 +62,8 @@ if (!in_array('downloads', $txpfile)) {
 }
 
 if (array_intersect(array('modified', 'created'), $txpfile)) {
-    safe_alter('txp_file', "
-        MODIFY modified DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
-        MODIFY created DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00'");
+    safe_alter('txp_file', "MODIFY modified DATETIME NOT NULL");
+    safe_alter('txp_file', "MODIFY created  DATETIME NOT NULL");
 }
 
 // Copy existing file timestamps into the new database columns.
