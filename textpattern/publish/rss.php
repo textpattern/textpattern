dbe478eecd83aee46439a634c8545889d237e180
etc <haut.buis@aliceadsl.fr>
2016-04-26 19:14:24 +0200

    Strip tags in rss title, as in 4.5

--- textpattern/publish/rss.php
+++ textpattern/publish/rss.php
@@ -156,7 +156,7 @@ function rss()
                     $count = '';
                 }
 
-                $Title = escape_title(preg_replace("/&(?![#a-z0-9]+;)/i", "&amp;", html_entity_decode($Title, ENT_QUOTES, 'UTF-8'))).$count;
+                $Title = escape_title(preg_replace("/&(?![#a-z0-9]+;)/i", "&amp;", html_entity_decode(strip_tags($Title), ENT_QUOTES, 'UTF-8'))).$count;
 
                 $thisauthor = get_author_name($AuthorID);
 
