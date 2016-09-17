605287ab0080abf9153de1d75eada0ef0084238e
Stef Dawson <thatbloke@stefdawson.com>
2016-09-17 23:54:48 +0100

    Move licence files programmatically

---
+++ textpattern/update/_to_4.7.0.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * Textpattern Content Management System
+ * http://textpattern.com
+ *
+ * Copyright (C) 2016 The Textpattern Development Team
+ *
+ * This file is part of Textpattern.
+ *
+ * Textpattern is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * Textpattern is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Textpattern. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+if (!defined('TXP_UPDATE')) {
+    exit("Nothing here. You can't access this file directly.");
+}
+
+// Move a few licence files. De-clutters the root directory a tad.
+foreach (array('LICENSE-BSD-3', 'LICENSE-LESSER') as $v) {
+    $orig = txpath.DS.'..'.DS.$v.'.txt';
+    $file = txpath.DS.'lib'.DS.$v.'.txt';
+
+    if (file_exists($orig)) {
+        @rename($orig, $file);
+    }
+}
+
+
