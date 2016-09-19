6d9a97e23d4c2288f87133a6e0091c0489b825c6
Phil Wareham <phil@designhive.com>
2016-09-19 14:55:53 +0100

    Re-bump theme versions

--- textpattern/admin-themes/hiveneutral/hiveneutral.php
+++ textpattern/admin-themes/hiveneutral/hiveneutral.php
@@ -215,7 +215,7 @@ EOS;
         return array(
             'title'       => 'Hive (Flat Neutral)',
             'description' => 'Textpattern Hive (Flat Neutral) admin theme',
-            'version'     => '4.6.0',
+            'version'     => '4.6.1-dev',
             'author'      => 'Phil Wareham',
             'author_uri'  => 'https://github.com/philwareham',
             'help'        => 'https://github.com/philwareham/textpattern-hive-admin-theme',
