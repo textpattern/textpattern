bb3e63558915dacc004c7f46703b3c7a2462f8ee
makss <makss@users.noreply.github.com>
2017-04-14 15:56:02 +0300

    txp_diag

--- textpattern/include/txp_diag.php
+++ textpattern/include/txp_diag.php
@@ -262,11 +262,7 @@ function doDiagnostics()
         $fail['dir_not_writable'] = join(n, $not_readable);
     }
 
-    if ($permlink_mode != 'messy' && !$is_apache) {
-        $fail['cleanurl_only_apache'] = diag_msg_wrap(gTxt('cleanurl_only_apache'), 'information');
-    }
-
-    if ($permlink_mode != 'messy' and !@is_readable($path_to_site.'/.htaccess')) {
+    if ($permlink_mode != 'messy' && $is_apache && !@is_readable($path_to_site.'/.htaccess')) {
         $fail['htaccess_missing'] = diag_msg_wrap(gTxt('htaccess_missing'));
     }
 
@@ -561,7 +557,7 @@ function doDiagnostics()
             ? n.gTxt('preflight_check').cs.n.ln.join("\n", doStripTags($fail)).n.ln
             : '',
 
-            (is_readable($path_to_site.'/.htaccess'))
+            ($is_apache && is_readable($path_to_site.'/.htaccess'))
             ?    n.gTxt('htaccess_contents').cs.n.ln.txpspecialchars(join('', file($path_to_site.'/.htaccess'))).n.ln
             :    '',
         );
