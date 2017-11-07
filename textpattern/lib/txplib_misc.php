4d6a32f381e3a9c646b2c45c4d07d89e867f140b
NicolasGraph <contact@nicolasmorand.com>
2017-11-07 17:11:05 +0100

    Resolves a conflict with the Thems branch

--- textpattern/lib/txplib_misc.php
+++ textpattern/lib/txplib_misc.php
@@ -6865,15 +6865,56 @@ function get_files_content($dir, $ext)
 
 function get_prefs_theme()
 {
-    //FIXME: After merge 'themes' branch
-
-    return array();
     $out = @json_decode(file_get_contents(txpath.'/setup/data/theme.prefs'), true);
     if (empty($out)) {
         return array();
     }
 
     return $out;
+}
+
+/**
+ * Gets the maximum allowed file upload size.
+ *
+ * Computes the maximum acceptable file size to the application if the
+ * user-selected value is larger than the maximum allowed by the current PHP
+ * configuration.
+ *
+ * @param  int $user_max Desired upload size supplied by the administrator
+ * @return int Actual value; the lower of user-supplied value or system-defined value
+ */
+
+function real_max_upload_size($user_max, $php = true)
+{
+    // The minimum of the candidates, is the real max. possible size
+    $candidates = $php ? array($user_max,
+        ini_get('post_max_size'),
+        ini_get('upload_max_filesize')
+    ) : array($user_max);
+    $real_max = null;
+
+    foreach ($candidates as $item) {
+        $val = floatval($item);
+        $modifier = strtolower(substr(trim($item), -1));
+
+        switch ($modifier) {
+            // The 'G' modifier is available since PHP 5.1.0
+            case 'g':
+                $val *= 1024;
+            case 'm':
+                $val *= 1024;
+            case 'k':
+                $val *= 1024;
+        }
+
+        if ($val >= 1) {
+            if (is_null($real_max) || $val < $real_max) {
+                $real_max = floor($val);
+            }
+        }
+    }
+
+    return $real_max;
 }
 
 /**
