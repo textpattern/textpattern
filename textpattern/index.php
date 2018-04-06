49d9d3676367ff717c3052cb7e5a0599538ab9a1
Stef Dawson <thatbloke@stefdawson.com>
2018-04-06 14:37:47 +0100

    This is 4.7.0-beta.3

--- textpattern/index.php
+++ textpattern/index.php
@@ -63,7 +63,7 @@ if (!defined('txpath')) {
 
 define("txpinterface", "admin");
 
-$thisversion = '4.7.0-dev';
+$thisversion = '4.7.0-beta.3';
 // $txp_using_svn deprecated in 4.7.0.
 $txp_using_svn = $txp_is_dev = true; // Set false for releases.
 
