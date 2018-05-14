6cec611726fea58ce206cd19945859338feecf9e
Pete Cooper <petecooper@users.noreply.github.com>
2018-05-14 10:06:43 +0100

    Update constants.php
    Revert half the PR.
    
    https://github.com/textpattern/textpattern/issues/1271#issuecomment-388745378

--- textpattern/lib/constants.php
+++ textpattern/lib/constants.php
@@ -71,6 +71,19 @@ define('VISIBLE', 1);
 
 define('RELOAD', -99);
 
+if (!defined('HELP_URL')) {		
+    /**		
+     * The location where help documentation is fetched.		
+     *		
+     * This constant can be overridden from the config.php.		
+     *		
+     * @example		
+     * define('HELP_URL', 'http://rpc.example.com/help/');		
+     */		
+		
+    define('HELP_URL', 'http://rpc.textpattern.com/help/');		
+}
+
 /**
  * Do not format text.
  *
