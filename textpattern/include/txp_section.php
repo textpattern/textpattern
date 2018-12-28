58c576afeefa0f34e0b318e478645d8f2c5c014a
etc <haut.buis@aliceadsl.fr>
2018-12-28 12:18:48 +0100

    Faulty strtolower fix

--- textpattern/include/txp_section.php
+++ textpattern/include/txp_section.php
@@ -626,15 +626,20 @@ function section_save()
     }
 
     // Prevent non-URL characters on section names.
-    $strtolower = function_exists('mb_strtolower') ? 'mb_strtolower' : 'strtolower';
-    $in['name'] = $strtolower(sanitizeForUrl($in['name']), 'UTF-8');
+    $mbstrings = extension_loaded('mbstrings');
+    $in['name'] = $mbstrings ?
+        mb_strtolower(sanitizeForUrl($in['name']), 'UTF-8') :
+        strtolower(sanitizeForUrl($in['name']));
 
     extract($in);
 
     $in = doSlash($in);
     extract($in, EXTR_PREFIX_ALL, 'safe');
+    $lower_name = $mbstrings ?
+        mb_strtolower($old_name, 'UTF-8') :
+        strtolower($old_name);
 
-    if ($name != $strtolower($old_name, 'UTF-8')) {
+    if ($name != $lower_name) {
         if (safe_field("name", 'txp_section', "name = '$safe_name'")) {
             // Invalid input. Halt all further processing (e.g. plugin event
             // handlers).
