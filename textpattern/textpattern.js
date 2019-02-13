8d7d6d48e577b3e3976ba96daf66b83ff5be7143
Phil Wareham <phil@designhive.com>
2019-02-13 11:13:06 +0000

    Consistent logic

--- textpattern/textpattern.js
+++ textpattern/textpattern.js
@@ -1553,7 +1553,7 @@ $(document).keydown(function (e) {
 
     if (key === 27) {
         $('.close').parent().toggle();
-    } else if (key === 19 || ((e.metaKey || e.ctrlKey) && !(e.altKey) && String.fromCharCode(key).toLowerCase() === 's'))
+    } else if (key === 19 || (!e.altKey && (e.metaKey || e.ctrlKey) && String.fromCharCode(key).toLowerCase() === 's'))
     {
         var obj = $('input.publish');
 
