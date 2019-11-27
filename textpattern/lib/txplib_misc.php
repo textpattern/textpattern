c8e9abaf8872fe31272b45ca56aed9c4704328ba
etc <haut.buis@aliceadsl.fr>
2019-11-27 20:40:42 +0100

    Yield tweaks

--- textpattern/lib/txplib_misc.php
+++ textpattern/lib/txplib_misc.php
@@ -1653,20 +1653,21 @@ function callback_handlers($event, $step = '', $pre = 0, $as_string = true)
 function lAtts($pairs, $atts, $warn = true)
 {
     global $pretext, $production_status, $txp_atts;
-    static $globals = null, $global_atts;
+    static $globals = null, $global_atts, $partial;
 
     if ($globals === null) {
         $global_atts = Txp::get('\Textpattern\Tag\Registry')->getRegistered(true);
         $globals = array_filter($global_atts);
+        $partial = Txp::get('\Textpattern\Tag\Registry')->getTag('yield');
     }
 
-    if (isset($txp_atts['yield']) && !isset($pairs['yield'])) {
+    if (isset($atts['yield']) && isset($txp_atts['yield']) && !isset($pairs['yield']) && $partial) {
         unset($atts['yield']);
 
         foreach (do_list_unique($txp_atts['yield']) as $name) {
-            $value = parse('<txp:yield name="'.txpspecialchars($name).'" />', true, false);
+            $value = call_user_func($partial, array('name' => $name));
 
-            if((string)$value !== '') {
+            if(isset($value)) {
                 $atts[$name] = $value;
             }
         }
