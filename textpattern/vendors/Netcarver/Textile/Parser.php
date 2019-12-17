0948cff0d3962330b40018a35beb21b1bae9619f
philwareham <phil@designhive.com>
2019-12-17 08:54:37 +0000

    Textile 3.7.5

--- textpattern/vendors/Netcarver/Textile/Parser.php
+++ textpattern/vendors/Netcarver/Textile/Parser.php
@@ -371,7 +371,7 @@ class Parser
      * @var string
      */
 
-    protected $ver = '3.7.4';
+    protected $ver = '3.7.5';
 
     /**
      * Regular expression snippets.
@@ -3079,7 +3079,7 @@ class Parser
                     }
                 }
 
-                if ($m['level'] > $prev['level'] && $m['st'] !== '') {
+                if ((!$prev || $m['level'] > $prev['level']) && $m['st'] !== '') {
                     $start = ' start="' . $this->olstarts[$m['tl']] . '"';
                 }
 
@@ -3088,7 +3088,7 @@ class Parser
                 }
             }
 
-            if (strpos($prev['tl'], ';') !== false && strpos($m['tl'], ':') !== false) {
+            if ($prev && $prev['tl'] && strpos($prev['tl'], ';') !== false && strpos($m['tl'], ':') !== false) {
                 $lists[$m['tl']] = 2;
             }
 
