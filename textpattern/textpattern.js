cce3831855131dcd13337d687ae9a77c0bcc4bda
Stef Dawson <thatbloke@stefdawson.com>
2019-12-21 17:44:51 +0000

    Put textpattern.js to dev as well

--- textpattern/textpattern.js
+++ textpattern/textpattern.js
@@ -27,7 +27,7 @@
  * Collection of client-side tools.
  */
 
-textpattern.version = '4.8.0-beta';
+textpattern.version = '4.8.0-dev';
 
 /**
  * Ascertain the page direction (LTR or RTL) as a variable.
