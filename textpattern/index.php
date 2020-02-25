f42cb6a0d0325b2930dd0b3ca697e58a520e852e
Stef Dawson <thatbloke@stefdawson.com>
2020-02-25 02:39:52 +0000

    Back to dev. Heading for 4.8.1

--- textpattern/index.php
+++ textpattern/index.php
@@ -27,9 +27,9 @@ if (!defined('txpath')) {
 
 define("txpinterface", "admin");
 
-$thisversion = '4.8.0';
+$thisversion = '4.8.1-dev';
 // $txp_using_svn deprecated in 4.7.0.
-$txp_using_svn = $txp_is_dev = false; // Set false for releases.
+$txp_using_svn = $txp_is_dev = true; // Set false for releases.
 
 ob_start(null, 2048);
 
