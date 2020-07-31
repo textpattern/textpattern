e2350002ce98a8e9e25c0212813238fb6891b475
Stef Dawson <thatbloke@stefdawson.com>
2020-07-31 10:13:15 +0100

    Remove script_js() from Files panel

--- textpattern/include/txp_file.php
+++ textpattern/include/txp_file.php
@@ -895,7 +895,7 @@ function file_create()
         $response .= 'textpattern.Console.addMessage('.json_encode($message, TEXTPATTERN_JSON).');'.n;
     }
 
-    script_js($response, false);
+    Txp::get('\Textpattern\UI\Script')->setContent($response, false);
     file_list();
 }
 
