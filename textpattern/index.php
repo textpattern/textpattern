a1f28a041b0a275561b3a1772f18b6ff228148a9
philwareham <phil@designhive.com>
2020-08-07 13:02:39 +0100

    Version number bump

--- textpattern/index.php
+++ textpattern/index.php
@@ -27,7 +27,7 @@ if (!defined('txpath')) {
 
 define("txpinterface", "admin");
 
-$thisversion = '4.8.2';
+$thisversion = '4.8.3';
 // $txp_using_svn deprecated in 4.7.0.
 $txp_using_svn = $txp_is_dev = false; // Set false for releases.
 
--- textpattern/textpattern.js
+++ textpattern/textpattern.js
@@ -27,7 +27,7 @@
  * Collection of client-side tools.
  */
 
-textpattern.version = '4.8.2';
+textpattern.version = '4.8.3';
 
 /**
  * Ascertain the page direction (LTR or RTL) as a variable.
