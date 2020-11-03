f1f90616195165bc029b3f213fc46612f28d4b4f
philwareham <phil@designhive.com>
2020-11-03 09:18:47 +0000

    Latest Hive admin themes

--- textpattern/admin-themes/hiveneutral/hiveneutral.php
+++ textpattern/admin-themes/hiveneutral/hiveneutral.php
@@ -32,6 +32,7 @@ class hiveNeutral_theme extends \Textpattern\Admin\Theme
         $out[] = '<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">';
         $out[] = '<link rel="stylesheet" href="'.$this->url.'assets/css/textpattern.css">';
         $out[] = '<link rel="icon" href="'.$this->url.'assets/img/favicon.ico">';
+        $out[] = '<meta name="color-scheme" content="dark light">';
         $out[] = '<meta name="generator" content="Textpattern CMS">';
         $out[] = '<script defer src="'.$this->url.'assets/js/main.js"></script>'.n;
 
