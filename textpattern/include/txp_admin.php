8747aec4112231ed6496ec2cd106e2b36be6f6ff
philwareham <phil@designhive.com>
2020-11-18 10:13:58 +0000

    Keyboard focus for responsive tables
    Fixes #1577

--- textpattern/include/txp_admin.php
+++ textpattern/include/txp_admin.php
@@ -400,7 +400,11 @@ function author_list($message = '')
                         'method' => 'post',
                         'action' => 'index.php',
                     )).
-                    n.tag_start('div', array('class' => 'txp-listtables')).
+                    n.tag_start('div', array(
+                        'class'      => 'txp-listtables',
+                        'tabindex'   => 0,
+                        'aria-label' => gTxt('list'),
+                    )).
                     n.tag_start('table', array('class' => 'txp-list')).
                     n.tag_start('thead').
                     tr(
