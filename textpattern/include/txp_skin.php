27980042cd491ff86bdf667c51c90d08f4a8e298
philwareham <phil@designhive.com>
2021-01-01 10:30:05 +0000

    Happy New Year

--- textpattern/include/txp_skin.php
+++ textpattern/include/txp_skin.php
@@ -5,7 +5,7 @@
  * https://textpattern.com/
  *
  * Copyright (C) 2005 Dean Allen
- * Copyright (C) 2020 The Textpattern Development Team
+ * Copyright (C) 2021 The Textpattern Development Team
  *
  * This file is part of Textpattern.
  *
