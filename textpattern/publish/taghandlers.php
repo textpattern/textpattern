f12512b1796159be6933450304635771169ffae2
etc <haut.buis@aliceadsl.fr>
2021-03-06 22:31:23 +0100

    <txp:variable />'s own default attribute

--- textpattern/publish/taghandlers.php
+++ textpattern/publish/taghandlers.php
@@ -5102,6 +5102,7 @@ function variable($atts, $thing = null)
         'escape'    => $set,
         'name'      => '',
         'value'     => null,
+        'default'   => false,
         'add'       => null,
         'reset'     => null,
         'separator' => null,
@@ -5134,13 +5135,17 @@ function variable($atts, $thing = null)
                 $value :
                 (isset($thing) ? parse($thing) : $var);
         }
+    }
 
-        if (isset($add)) {
-            if (!isset($separator) && is_numeric($add) && (empty($var) || is_numeric($var))) {
-                $var += $add;
-            } else {
-                $var .= ($var ? $separator : '').$add;
-            }
+    if ($default !== false && trim($var) === '') {
+        $var = $default;
+    }
+
+    if (isset($add)) {
+        if (!isset($separator) && is_numeric($add) && (empty($var) || is_numeric($var))) {
+            $var += $add;
+        } else {
+            $var .= ($var ? $separator : '').$add;
         }
     }
 
