925dcc33a53ad054456cdb72742746b457fac5da
Pete Cooper <petecooper@users.noreply.github.com>
2021-03-30 11:43:42 +0100

    Update index.php

--- textpattern/tmp/index.php
+++ textpattern/tmp/index.php
@@ -1,1 +1,10 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+  </head>
+  <body>
+  <!-- empty index.php to deter access or directory listings -->
+  </body>
+</html>
