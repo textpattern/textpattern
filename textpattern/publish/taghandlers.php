3bef7bfd7a492700dc6db52987c3a9334b03e56c
etc <haut.buis@aliceadsl.fr>
2021-04-01 11:17:57 +0200

    Fixes #1670

--- textpattern/publish/taghandlers.php
+++ textpattern/publish/taghandlers.php
@@ -5054,7 +5054,7 @@ function variable($atts, $thing = null)
 {
     global $variable, $trace;
 
-    $set = isset($thing) || isset($atts['value']) || isset($atts['add']) || isset($atts['reset']) ? '' : null;
+    $set = isset($thing) || isset($atts['value']) || isset($atts['add']) || isset($atts['reset']) ? false : null;
 
     extract(lAtts(array(
         'escape'    => $set,
@@ -5064,7 +5064,10 @@ function variable($atts, $thing = null)
         'add'       => null,
         'reset'     => null,
         'separator' => null,
-        'output'    => null
+        'output'    => null,
+        'break'     => $set,
+        'trim'      => $set,
+        'replace'   => $set
     ), $atts));
 
     $var = isset($variable[$name]) ? $variable[$name] : null;
@@ -5108,6 +5111,10 @@ function variable($atts, $thing = null)
     }
 
     if ($set !== null) {
+        if ($break !== false || $trim !== false || $replace !== false) {
+            $var = txp_wraptag(compact('break', 'trim', 'replace'), $var);
+        }
+
         $var = $escape ? txp_escape(array('escape' => $escape), $var) : $var;
 
         if (isset($reset)) {
