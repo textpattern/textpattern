33f46accf3d5b101c335db33360890f1d6c50bf9
philwareham <phil@designhive.com>
2021-12-14 09:35:17 +0000

    Silence a CI warning pt2

--- textpattern/admin-themes/hive/assets/js/main.js
+++ textpattern/admin-themes/hive/assets/js/main.js
@@ -1,1 +1,2 @@
+/** global: localStorage */
 !function(){"use strict";var t=document.body.classList,e=document.getElementById("lightswitch");null!==e&&e.addEventListener("click",function(e){t.contains("darkmode")?(t.remove("darkmode"),localStorage.setItem("prefers-color-scheme","light")):(t.add("darkmode"),localStorage.setItem("prefers-color-scheme","dark")),e.preventDefault()})}();
