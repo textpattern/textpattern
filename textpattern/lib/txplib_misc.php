6538ab2b064dadb0ad86a4b378dc3f93a1085052
Stef Dawson <thatbloke@stefdawson.com>
2022-01-31 14:38:18 +0000

    Remove some (gm)strftime() instances in txplib_misc

--- textpattern/lib/txplib_misc.php
+++ textpattern/lib/txplib_misc.php
@@ -2789,11 +2789,11 @@ function safe_strftime($format, $time = null, $gmt = false, $override_locale = '
     }
 
     if ($gmt) {
-        $str = gmstrftime($format, $time);
+        $str = gmdate(strtr($format, $translate), $time);
     } else {
         $tztime = $time + tz_offset($time);
         $format = str_replace('%s', $tztime, $format);
-        $str = strftime($format, $tztime);
+        $str = date(strtr($format, $translate), $tztime);
     }
 
     if (!isset($charsets[$override_locale])) {
