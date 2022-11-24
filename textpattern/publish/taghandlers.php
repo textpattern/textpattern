0b95afcab984759f97029d9dd5929246efd2209c
etc <haut.buis@aliceadsl.fr>
2022-11-24 17:35:34 +0100

    Update taghandlers.php

--- textpattern/publish/taghandlers.php
+++ textpattern/publish/taghandlers.php
@@ -5103,7 +5103,7 @@ function if_request($atts, $thing = null)
 function txp_eval($atts, $thing = null)
 {
     global $prefs, $txp_tag, $txp_atts;
-    static $xpath = null, $functions = null;
+    static $xpath = null, $functions = null, $_functions = null;
 
     unset($txp_atts['evaluate']);
     $staged = null;
