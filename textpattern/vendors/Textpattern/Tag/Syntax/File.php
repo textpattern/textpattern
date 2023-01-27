f8cb38a6f257cf18a942e838984c35e8b2be6eef
etc <haut.buis@aliceadsl.fr>
2023-01-27 16:08:58 +0100

    Transfer file and link tags to their class
    will be loaded only if needed

--- textpattern/vendors/Textpattern/Tag/Syntax/File.php
+++ textpattern/vendors/Textpattern/Tag/Syntax/File.php
@@ -24,44 +24,351 @@
 /**
  * File tags.
  *
- * @since  4.6.0
+ * @since  4.9.0
  */
 
 namespace Textpattern\Tag\Syntax;
 
 class File
 {
-    /**
-     * Checks if the file is the first in the list.
-     *
-     * @param  array  $atts
-     * @param  string $thing
-     * @return string
-     */
-
-    public static function renderIfFirstFile($atts, $thing)
+    public static function file_download_list($atts, $thing = null)
     {
-        global $thisfile;
-
-        assert_file();
-
-        return parse($thing, !empty($thisfile['is_first']));
+        global $s, $c, $context, $thisfile, $thispage, $pretext;
+    
+        extract(lAtts(array(
+            'break'       => br,
+            'category'    => '',
+            'author'      => '',
+            'realname'    => '',
+            'auto_detect' => 'category, author',
+            'class'       => 'file_download_list',
+            'form'        => isset($thing) ? '' : 'files',
+            'id'          => '',
+            'pageby'      => '',
+            'limit'       => 10,
+            'offset'      => 0,
+            'month'       => '',
+            'time'        => null,
+            'sort'        => 'filename asc',
+            'wraptag'     => '',
+            'status'      => STATUS_LIVE,
+        ), $atts));
+    
+        if (!is_numeric($status)) {
+            $status = getStatusNum($status);
+        }
+    
+        // Note: status treated slightly differently.
+        $where = array();
+        $filters = isset($atts['id']) || isset($atts['category']) || isset($atts['author']) || isset($atts['realname']) || isset($atts['status']);
+        $context_list = (empty($auto_detect) || $filters) ? array() : do_list_unique($auto_detect);
+        $pageby = ($pageby == 'limit') ? $limit : $pageby;
+    
+        if ($category) {
+            $where[] = "category IN ('".join("','", doSlash(do_list_unique($category)))."')";
+        }
+    
+        $ids = $id ? array_map('intval', do_list_unique($id, array(',', '-'))) : array();
+    
+        if ($ids) {
+            $where[] = "id IN ('".join("','", $ids)."')";
+        }
+    
+        if ($author) {
+            $where[] = "author IN ('".join("','", doSlash(do_list_unique($author)))."')";
+        }
+    
+        if ($realname) {
+            $authorlist = safe_column("name", 'txp_users', "RealName IN ('".join("','", doArray(doSlash(do_list_unique($realname)), 'urldecode'))."')");
+            if ($authorlist) {
+                $where[] = "author IN ('".join("','", doSlash($authorlist))."')";
+            }
+        }
+    
+        // If no files are selected, try...
+        if (!$where && !$filters) {
+            foreach ($context_list as $ctxt) {
+                switch ($ctxt) {
+                    case 'category':
+                        // ...the global category in the URL.
+                        if ($context == 'file' && !empty($c)) {
+                            $where[] = "category = '".doSlash($c)."'";
+                        }
+                        break;
+                    case 'author':
+                        // ...the global author in the URL.
+                        if ($context == 'file' && !empty($pretext['author'])) {
+                            $where[] = "author = '".doSlash($pretext['author'])."'";
+                        }
+                        break;
+                }
+    
+                // Only one context can be processed.
+                if ($where) {
+                    break;
+                }
+            }
+        }
+    
+        if ($status) {
+            $where[] = "status = '".doSlash($status)."'";
+        } elseif (!$where && $filters) {
+            // If nothing matches, output nothing.
+            return '';
+        }
+    
+        if ($time === null || $time || $month) {
+            $where[] = buildTimeSql($month, $time === null ? 'past' : $time, 'created');
+        }
+    
+        $where = join(" AND ", $where);
+    
+        // Set up paging if required.
+        if ($limit && $pageby) {
+            $pg = (!$pretext['pg']) ? 1 : $pretext['pg'];
+            $pgoffset = $offset + (($pg - 1) * $pageby);
+    
+            if (empty($thispage)) {
+                $grand_total = safe_count('txp_file', $where);
+                $total = $grand_total - $offset;
+                $numPages = ($pageby > 0) ? ceil($total/$pageby) : 1;
+    
+                // Send paging info to txp:newer and txp:older.
+                $pageout['pg']          = $pg;
+                $pageout['numPages']    = $numPages;
+                $pageout['s']           = $s;
+                $pageout['c']           = $c;
+                $pageout['context']     = 'file';
+                $pageout['grand_total'] = $grand_total;
+                $pageout['total']       = $total;
+                $thispage = $pageout;
+            }
+        } else {
+            $pgoffset = $offset;
+        }
+    
+        // Preserve order of custom file ids unless 'sort' attribute is set.
+        if (!empty($ids) && empty($atts['sort'])) {
+            $safe_sort = "FIELD(id, ".join(',', $ids).")";
+        } else {
+            $safe_sort = sanitizeForSort($sort);
+        }
+    
+        $qparts = array(
+            "ORDER BY ".$safe_sort,
+            ($limit) ? "LIMIT ".intval($pgoffset).", ".intval($limit) : '',
+        );
+    
+        $rs = safe_rows_start("*", 'txp_file', $where.' '.join(' ', $qparts));
+        $out = parseList($rs, $thisfile, 'file_download_format_info', compact('form', 'thing'));
+    
+        return $out ? doWrap($out, $wraptag, compact('break', 'class')) : '';
     }
-
-    /**
-     * Checks if the file is the last in the list.
-     *
-     * @param  array  $atts
-     * @param  string $thing
-     * @return string
-     */
-
-    public static function renderIfLastFile($atts, $thing)
+    
+    // -------------------------------------------------------------
+    
+    public static function file_download($atts, $thing = null)
     {
         global $thisfile;
-
+    
+        extract(lAtts(array(
+            'filename' => '',
+            'form'     => 'files',
+            'id'       => '',
+        ), $atts));
+    
+        $from_form = false;
+    
+        if ($id) {
+            $thisfile = fileDownloadFetchInfo('id = '.intval($id).' and created <= '.now('created'));
+        } elseif ($filename) {
+            $thisfile = fileDownloadFetchInfo("filename = '".doSlash($filename)."' and created <= ".now('created'));
+        } else {
+            assert_file();
+    
+            $from_form = true;
+        }
+    
+        if ($thisfile) {
+            $out = ($thing) ? parse($thing) : parse_form($form);
+    
+            // Cleanup: this wasn't called from a form, so we don't want this
+            // value remaining.
+            if (!$from_form) {
+                $thisfile = '';
+            }
+    
+            return $out;
+        }
+    }
+    
+    // -------------------------------------------------------------
+    
+    public static function file_download_link($atts, $thing = null)
+    {
+        global $thisfile;
+    
+        extract(lAtts(array(
+            'filename' => '',
+            'id'       => '',
+        ), $atts));
+    
+        $from_form = false;
+    
+        if ($id) {
+            $thisfile = fileDownloadFetchInfo('id = '.intval($id).' and created <= '.now('created'));
+        } elseif ($filename) {
+            $thisfile = fileDownloadFetchInfo("filename = '".doSlash($filename)."' and created <= ".now('created'));
+        } else {
+            assert_file();
+    
+            $from_form = true;
+        }
+    
+        if ($thisfile) {
+            $url = filedownloadurl($thisfile['id'], $thisfile['filename']);
+    
+            $out = ($thing) ? href(parse($thing), $url) : $url;
+    
+            // Cleanup: this wasn't called from a form, so we don't want this
+            // value remaining
+            if (!$from_form) {
+                $thisfile = '';
+            }
+    
+            return $out;
+        }
+    }
+    
+    // -------------------------------------------------------------
+    
+    public static function file_download_size($atts)
+    {
+        global $thisfile;
+    
+        extract(lAtts(array(
+            'decimals' => 2,
+            'format'   => '',
+        ), $atts));
+    
         assert_file();
-
-        return parse($thing, !empty($thisfile['is_last']));
+    
+        if (is_numeric($decimals) && $decimals >= 0) {
+            $decimals = intval($decimals);
+        } else {
+            $decimals = 2;
+        }
+    
+        if (isset($thisfile['size'])) {
+            $format_unit = strtolower(substr($format, 0, 1));
+    
+            return format_filesize($thisfile['size'], $decimals, $format_unit);
+        } else {
+            return '';
+        }
+    }
+    
+    // -------------------------------------------------------------
+    
+    public static function file_download_id()
+    {
+        global $thisfile;
+    
+        assert_file();
+    
+        return $thisfile['id'];
+    }
+    
+    // -------------------------------------------------------------
+    
+    public static function file_download_name($atts)
+    {
+        global $thisfile;
+    
+        extract(lAtts(array('title' => 0), $atts));
+    
+        assert_file();
+    
+        return ($title) ? $thisfile['title'] : $thisfile['filename'];
+    }
+    
+    // -------------------------------------------------------------
+    
+    public static function file_download_category($atts)
+    {
+        global $thisfile;
+    
+        extract(lAtts(array('title' => 0), $atts));
+    
+        assert_file();
+    
+        if ($thisfile['category']) {
+            $category = ($title)
+                ? fetch_category_title($thisfile['category'], 'file')
+                : $thisfile['category'];
+    
+            return $category;
+        }
+    }
+    
+    // -------------------------------------------------------------
+    
+    public static function file_download_author($atts)
+    {
+        global $thisfile, $s;
+    
+        extract(lAtts(array(
+            'link'         => 0,
+            'title'        => 1,
+            'section'      => '',
+            'this_section' => '',
+        ), $atts));
+    
+        assert_file();
+    
+        if ($thisfile['author']) {
+            $author_name = get_author_name($thisfile['author']);
+            $display_name = txpspecialchars(($title) ? $author_name : $thisfile['author']);
+    
+            $section = ($this_section) ? ($s == 'default' ? '' : $s) : $section;
+    
+            $author = ($link)
+                ? href($display_name, pagelinkurl(array(
+                    's'       => $section,
+                    'author'  => $author_name,
+                    'context' => 'file',
+                )))
+                : $display_name;
+    
+            return $author;
+        }
+    }
+    
+    // -------------------------------------------------------------
+    
+    public static function file_download_downloads()
+    {
+        global $thisfile;
+    
+        assert_file();
+    
+        return $thisfile['downloads'];
+    }
+    
+    // -------------------------------------------------------------
+    
+    public static function file_download_description($atts)
+    {
+        global $thisfile;
+    
+        extract(lAtts(array('escape' => null), $atts));
+    
+        assert_file();
+    
+        if ($thisfile['description']) {
+            return ($escape === null)
+                ? txpspecialchars($thisfile['description'])
+                : $thisfile['description'];
+        }
     }
 }
--- textpattern/vendors/Textpattern/Tag/Syntax/Link.php
+++ textpattern/vendors/Textpattern/Tag/Syntax/Link.php
@@ -24,44 +24,305 @@
 /**
  * Link tags.
  *
- * @since  4.6.0
+ * @since  4.9.0
  */
 
 namespace Textpattern\Tag\Syntax;
 
 class Link
 {
-    /**
-     * Checks if the link is the first in the list.
-     *
-     * @param  array  $atts
-     * @param  string $thing
-     * @return string
-     */
-
-    public static function renderIfFirstLink($atts, $thing)
+    public static function linklist($atts, $thing = null)
     {
-        global $thislink;
-
-        assert_link();
-
-        return parse($thing, !empty($thislink['is_first']));
+        global $s, $c, $context, $thislink, $thispage, $pretext;
+    
+        extract(lAtts(array(
+            'break'       => '',
+            'category'    => '',
+            'author'      => '',
+            'realname'    => '',
+            'auto_detect' => 'category, author',
+            'class'       => 'linklist',
+            'form'        => isset($thing) ? '' : 'plainlinks',
+            'id'          => '',
+            'pageby'      => '',
+            'limit'       => 0,
+            'offset'      => 0,
+            'month'       => '',
+            'time'        => null,
+            'sort'        => 'linksort asc',
+            'wraptag'     => '',
+        ), $atts));
+    
+        $where = array();
+        $filters = isset($atts['category']) || isset($atts['author']) || isset($atts['realname']);
+        $context_list = (empty($auto_detect) || $filters) ? array() : do_list_unique($auto_detect);
+        $pageby = ($pageby == 'limit') ? $limit : $pageby;
+    
+        if ($category) {
+            $where[] = "category IN ('".join("','", doSlash(do_list_unique($category)))."')";
+        }
+    
+        if ($id) {
+            $where[] = "id IN ('".join("','", doSlash(do_list_unique($id, array(',', '-'))))."')";
+        }
+    
+        if ($author) {
+            $where[] = "author IN ('".join("','", doSlash(do_list_unique($author)))."')";
+        }
+    
+        if ($realname) {
+            $authorlist = safe_column("name", 'txp_users', "RealName IN ('".join("','", doArray(doSlash(do_list_unique($realname)), 'urldecode'))."')");
+            if ($authorlist) {
+                $where[] = "author IN ('".join("','", doSlash($authorlist))."')";
+            }
+        }
+    
+        // If no links are selected, try...
+        if (!$where && !$filters) {
+            foreach ($context_list as $ctxt) {
+                switch ($ctxt) {
+                    case 'category':
+                        // ...the global category in the URL.
+                        if ($context == 'link' && !empty($c)) {
+                            $where[] = "category = '".doSlash($c)."'";
+                        }
+                        break;
+                    case 'author':
+                        // ...the global author in the URL.
+                        if ($context == 'link' && !empty($pretext['author'])) {
+                            $where[] = "author = '".doSlash($pretext['author'])."'";
+                        }
+                        break;
+                }
+    
+                // Only one context can be processed.
+                if ($where) {
+                    break;
+                }
+            }
+        }
+    
+        if (!$where && $filters) {
+            // If nothing matches, output nothing.
+            return '';
+        }
+    
+        if ($time === null || $time || $month) {
+            $where[] = buildTimeSql($month, $time === null ? 'past' : $time, 'date');
+        }
+    
+        if (!$where) {
+            // If nothing matches, start with all links.
+            $where[] = "1 = 1";
+        }
+    
+        $where = join(" AND ", $where);
+    
+        // Set up paging if required.
+        if ($limit && $pageby) {
+            $pg = (!$pretext['pg']) ? 1 : $pretext['pg'];
+            $pgoffset = $offset + (($pg - 1) * $pageby);
+    
+            if (empty($thispage)) {
+                $grand_total = safe_count('txp_link', $where);
+                $total = $grand_total - $offset;
+                $numPages = ($pageby > 0) ? ceil($total/$pageby) : 1;
+    
+                // Send paging info to txp:newer and txp:older.
+                $pageout['pg']          = $pg;
+                $pageout['numPages']    = $numPages;
+                $pageout['s']           = $s;
+                $pageout['c']           = $c;
+                $pageout['context']     = 'link';
+                $pageout['grand_total'] = $grand_total;
+                $pageout['total']       = $total;
+                $thispage = $pageout;
+            }
+        } else {
+            $pgoffset = $offset;
+        }
+    
+        $qparts = array(
+            $where,
+            'ORDER BY '.sanitizeForSort($sort),
+            ($limit) ? 'LIMIT '.intval($pgoffset).', '.intval($limit) : '',
+        );
+    
+        $rs = safe_rows_start("*, UNIX_TIMESTAMP(date) AS uDate", 'txp_link', join(' ', $qparts));
+        $out = parseList($rs, $thislink, function($a) {
+            global $thislink;
+            $thislink = $a;
+            $thislink['date'] = $thislink['uDate'];
+            unset($thislink['uDate']);
+        }, compact('form', 'thing'));
+    
+        return $out ? doWrap($out, $wraptag, $break, $class) : '';
     }
-
-    /**
-     * Checks if the link is the last in the list.
-     *
-     * @param  array  $atts
-     * @param  string $thing
-     * @return string
-     */
-
-    public static function renderIfLastLink($atts, $thing)
+    
+    // -------------------------------------------------------------
+    
+    // NOTE: tpt_ prefix used because link() is a PHP function. See publish.php.
+    public static function link($atts)
     {
         global $thislink;
-
+    
+        extract(lAtts(array(
+            'rel'    => '',
+            'id'     => '',
+            'name'   => '',
+            'escape' => true,
+        ), $atts));
+    
+        $rs = $thislink;
+        $sql = array();
+    
+        if ($id) {
+            $sql[] = "id = ".intval($id);
+        } elseif ($name) {
+            $sql[] = "linkname = '".doSlash($name)."'";
+        }
+    
+        if ($sql) {
+            $rs = safe_row("linkname, url", 'txp_link', implode(" AND ", $sql)." LIMIT 1");
+        }
+    
+        if (!$rs) {
+            trigger_error(gTxt('unknown_link'));
+    
+            return '';
+        }
+    
+        return tag(
+            $escape ? txp_escape($escape, $rs['linkname']) : $rs['linkname'], 'a',
+            ($rel ? ' rel="'.txpspecialchars($rel).'"' : '').
+            ' href="'.txpspecialchars($rs['url']).'"'
+        );
+    }
+    
+    // -------------------------------------------------------------
+    
+    public static function linkdesctitle($atts)
+    {
+        global $thislink;
+    
+        extract(lAtts(array('rel' => '', 'escape' => true), $atts));
+    
         assert_link();
-
-        return parse($thing, !empty($thislink['is_last']));
+    
+        $description = ($thislink['description'])
+            ? ' title="'.txpspecialchars($thislink['description']).'"'
+            : '';
+    
+        return tag(
+            $escape ? txp_escape($escape, $thislink['linkname']) : $thislink['linkname'], 'a',
+            ($rel ? ' rel="'.txpspecialchars($rel).'"' : '').
+            ' href="'.doSpecial($thislink['url']).'"'.$description
+        );
+    }
+    
+    // -------------------------------------------------------------
+    
+    public static function link_name($atts)
+    {
+        global $thislink;
+    
+        extract(lAtts(array('escape' => null), $atts));
+    
+        assert_link();
+    
+        return ($escape === null)
+            ? txpspecialchars($thislink['linkname'])
+            : $thislink['linkname'];
+    }
+    
+    // -------------------------------------------------------------
+    
+    public static function link_url()
+    {
+        global $thislink;
+    
+        assert_link();
+    
+        return doSpecial($thislink['url']);
+    }
+    
+    // -------------------------------------------------------------
+    
+    public static function link_author($atts)
+    {
+        global $thislink, $s;
+    
+        extract(lAtts(array(
+            'link'         => 0,
+            'title'        => 1,
+            'section'      => '',
+            'this_section' => '',
+        ), $atts));
+    
+        assert_link();
+    
+        if ($thislink['author']) {
+            $author_name = get_author_name($thislink['author']);
+            $display_name = txpspecialchars(($title) ? $author_name : $thislink['author']);
+    
+            $section = ($this_section) ? ($s == 'default' ? '' : $s) : $section;
+    
+            $author = ($link)
+                ? href($display_name, pagelinkurl(array(
+                    's'       => $section,
+                    'author'  => $author_name,
+                    'context' => 'link',
+                )))
+                : $display_name;
+    
+            return $author;
+        }
+    }
+    
+    // -------------------------------------------------------------
+    
+    public static function link_description($atts)
+    {
+        global $thislink;
+    
+        extract(lAtts(array('escape' => null), $atts));
+    
+        assert_link();
+    
+        if ($thislink['description']) {
+            return ($escape === null) ?
+                txpspecialchars($thislink['description']) :
+                $thislink['description'];
+        }
+    }
+    
+    // -------------------------------------------------------------
+    
+    public static function link_category($atts)
+    {
+        global $thislink;
+    
+        extract(lAtts(array('title' => 0), $atts));
+    
+        assert_link();
+    
+        if ($thislink['category']) {
+            $category = ($title)
+                ? fetch_category_title($thislink['category'], 'link')
+                : $thislink['category'];
+    
+            return $category;
+        }
+    }
+    
+    // -------------------------------------------------------------
+    
+    public static function link_id()
+    {
+        global $thislink;
+    
+        assert_link();
+    
+        return $thislink['id'];
     }
 }
