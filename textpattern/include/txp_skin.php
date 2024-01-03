9c4c3cc644fd74d6df33dddb9346f024f63201fe
Phil Wareham <phil@designhive.com>
2024-01-03 08:25:20 +0000

    Happy New Year 2024

--- textpattern/include/txp_skin.php
+++ textpattern/include/txp_skin.php
@@ -5,7 +5,7 @@
  * https://textpattern.com/
  *
  * Copyright (C) 2005 Dean Allen
- * Copyright (C) 2023 The Textpattern Development Team
+ * Copyright (C) 2024 The Textpattern Development Team
  *
  * This file is part of Textpattern.
  *
