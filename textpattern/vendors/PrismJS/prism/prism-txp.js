523e9c8ceb8cab93004d2a988eab1cf521a99e7d
etc <haut.buis@aliceadsl.fr>
2024-11-06 23:16:08 +0100

    Update prism-txp.js

--- textpattern/vendors/PrismJS/prism/prism-txp.js
+++ textpattern/vendors/PrismJS/prism/prism-txp.js
@@ -1,5 +1,5 @@
 Prism.languages.markup = {txp: {...Prism.languages.markup.tag}, ...Prism.languages.markup};
-Prism.languages.markup.txp.pattern=/<\/?(?:txp|[a-z]+:):[\w\-\x80-\xffff]+(?:\[-?\d+\])?(?:\s+\$?[\w\-\x80-\xffff]+(?:\s*=\s*(?:(?<d>"+)(?:[^"]|\k<d>")*\k<d>|(?<s>\'+)(?:[^\']|\k<s>\')*\k<s>|[^\s\'"\/>]+))?)*\s*\/?(?<!\-\-)\>/s;
-Prism.languages.markup.txp.inside['attr-value'].pattern=/=\s*(?:(?<d>"+)(?:[^"]|\k<d>")*\k<d>|(?<s>\'+)(?:[^\']|\k<s>\')*\k<s>|[^\s\'"\/>]+)/s;
+Prism.languages.markup.txp.pattern=/<\/?(?:txp|[a-z]+:):[\w\-\x80-\xffff]+(?:\[-?\d+\])?(?:\s+\$?[\w\-\x80-\xffff]+(?:\s*=\s*(?:(?<d>"+)(?:[^"]|\k<d>{2})*\k<d>|(?<s>\'+)(?:[^\']|\k<s>{2})*\k<s>|[^\s\'"\/>]+))?)*\s*\/?(?<!\-\-)\>/s;
+Prism.languages.markup.txp.inside['attr-value'].pattern=/=\s*(?:(?<d>"+)(?:[^"]|\k<d>{2})*\k<d>|(?<s>\'+)(?:[^\']|\k<s>{2})*\k<s>|[^\s\'"\/>]+)/s;
 Prism.languages.markup.txp.inside['attr-value'].greedy = false;
 Prism.languages.markup.txp.inside['attr-value'].inside.txp = Prism.languages.markup.txp;
