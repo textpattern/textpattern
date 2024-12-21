85f6652379fcca02899e70a4495d2d1123a75adf
Pete Cooper <pete@apropos.local>
2024-12-21 12:56:19 +0000

    phpcs: formatting / indents / spacing; no code changes tested, works as expected ping https://github.com/textpattern/textpattern/issues/1965

--- .github/txp-index.php
+++ .github/txp-index.php
@@ -1,4 +1,5 @@
 <?php
+
 $gitview = 'https://github.com/textpattern/textpattern/blob';
 $outdir = '.github/index';
 
@@ -59,89 +60,89 @@ $outdir = '.github/index';
     index_file_save('gtxt_lang_not_used', array_keys($lang_gtxt));
 
 
-exit;
+    exit;
 
-function lang_gtxt($filename)
-{
-    $txt = file_get_contents($filename);
-    if (preg_match_all('%^(\w+)\s+=>\s+(.*)$%m', $txt, $mm)) {
-        return array_flip($mm[1]);
+    function lang_gtxt($filename)
+    {
+        $txt = file_get_contents($filename);
+        if (preg_match_all('%^(\w+)\s+=>\s+(.*)$%m', $txt, $mm)) {
+            return array_flip($mm[1]);
+        }
+        return array();
     }
-    return array();
-}
 
 
-function index_code_gtxt($files)
-{
-    $gtxt_ok = array();
-    $gtxt_bad = array();
+    function index_code_gtxt($files)
+    {
+        $gtxt_ok = array();
+        $gtxt_bad = array();
 
-    foreach ($files as $file) {
-        $txt = file_get_contents($file);
-        if (preg_match_all('%gTxt\((?:\s+)?(.*?)[\)\,]%s', $txt, $mm)) {
-            foreach ($mm[1] as $m) {
-                if (preg_match('%^[\'\"]([\w\-]+)[\'\"]$%', trim($m), $nn)) {
-                    @$gtxt_ok[$nn[1]]++;
-                } elseif (preg_match("% \? '([\w\-]+)' : '([\w\-]+)'%", trim($m), $nn)) {
-                    @$gtxt_ok[$nn[1]]++;
-                    @$gtxt_ok[$nn[2]]++;
-                } else {
-                    $gtxt_bad[$m] = $file;
+        foreach ($files as $file) {
+            $txt = file_get_contents($file);
+            if (preg_match_all('%gTxt\((?:\s+)?(.*?)[\)\,]%s', $txt, $mm)) {
+                foreach ($mm[1] as $m) {
+                    if (preg_match('%^[\'\"]([\w\-]+)[\'\"]$%', trim($m), $nn)) {
+                        @$gtxt_ok[$nn[1]]++;
+                    } elseif (preg_match("% \? '([\w\-]+)' : '([\w\-]+)'%", trim($m), $nn)) {
+                        @$gtxt_ok[$nn[1]]++;
+                        @$gtxt_ok[$nn[2]]++;
+                    } else {
+                        $gtxt_bad[$m] = $file;
+                    }
                 }
             }
         }
+
+        ksort($gtxt_ok);
+        ksort($gtxt_bad);
+        index_file_save('gtxt_parse_bad', print_r($gtxt_bad, true));
+        return $gtxt_ok;
     }
 
-    ksort($gtxt_ok);
-    ksort($gtxt_bad);
-    index_file_save('gtxt_parse_bad', print_r($gtxt_bad, true));
-    return $gtxt_ok;
-}
 
+    function index_code($search)
+    {
+        global $gitview, $branch;
+        $out = array();
+        $out2 = array();
+        $txt = shell_exec('grep --exclude-dir={.github,sites} --include=\*.php -rn . -e "' . escapeshellcmd($search) . '("');
 
-function index_code($search)
-{
-    global $gitview, $branch;
-    $out = array();
-    $out2 = array();
-    $txt = shell_exec('grep --exclude-dir={.github,sites} --include=\*.php -rn . -e "'.escapeshellcmd($search).'("');
-
-    if (preg_match_all('%^\./(.*?):(.*?):(?:\s+)?(.*?'.preg_quote($search).'\((.*))$%ium', $txt, $mm)) {
-        foreach ($mm[3] as $key=>$d) {
-            if (! preg_match('%^(\*|function)%', trim($d))) {
-                $d = trim(preg_replace('%(<\?php|\?\>)%', '', $d));
-                $k = preg_replace('%[\,\)].*$%', '', $mm[4][$key]);
-                $out["{$k}_{$key}"] = "<tr><td>{$k}</td><td>{$d}</td><td><a href='{$gitview}/{$branch}/{$mm[1][$key]}#L{$mm[2][$key]}'>{$mm[1][$key]}</a></td></tr>";
+        if (preg_match_all('%^\./(.*?):(.*?):(?:\s+)?(.*?' . preg_quote($search) . '\((.*))$%ium', $txt, $mm)) {
+            foreach ($mm[3] as $key => $d) {
+                if (! preg_match('%^(\*|function)%', trim($d))) {
+                    $d = trim(preg_replace('%(<\?php|\?\>)%', '', $d));
+                    $k = preg_replace('%[\,\)].*$%', '', $mm[4][$key]);
+                    $out["{$k}_{$key}"] = "<tr><td>{$k}</td><td>{$d}</td><td><a href='{$gitview}/{$branch}/{$mm[1][$key]}#L{$mm[2][$key]}'>{$mm[1][$key]}</a></td></tr>";
+                }
             }
+            ksort($out);
+            index_file_save_html($search, '<table class="idx">' . join("\n", $out) . "\n" . '</table>');
         }
-        ksort($out);
-        index_file_save_html($search, '<table class="idx">'.join("\n", $out)."\n".'</table>');
     }
-}
 
 
-function index_get_branch()
-{
-    $txt = @file_get_contents('.git/HEAD');
-    if (preg_match('%refs/heads/(.*)$%m', $txt, $mm)) {
-        return $mm[1];
+    function index_get_branch()
+    {
+        $txt = @file_get_contents('.git/HEAD');
+        if (preg_match('%refs/heads/(.*)$%m', $txt, $mm)) {
+            return $mm[1];
+        }
+        return '';
     }
-    return '';
-}
 
-function index_file_save($name, $data)
-{
-    global $branch, $outdir;
-    if (is_array($data)) {
-        $data = join("\n", $data)."\n";
+    function index_file_save($name, $data)
+    {
+        global $branch, $outdir;
+        if (is_array($data)) {
+            $data = join("\n", $data) . "\n";
+        }
+        file_put_contents("{$outdir}/{$branch}__{$name}.txt", $data);
     }
-    file_put_contents("{$outdir}/{$branch}__{$name}.txt", $data);
-}
 
-function index_file_save_html($name, $data)
-{
-    global $branch, $outdir;
-$head =<<<EOF
+    function index_file_save_html($name, $data)
+    {
+        global $branch, $outdir;
+        $head = <<<EOF
 <!DOCTYPE html><html lang="en-gb" dir="ltr">
 <head>
     <meta charset="utf-8">
@@ -154,14 +155,14 @@ body { background-color: #fafafa; }
 </head>
 <body>
 EOF;
-    file_put_contents("{$outdir}/{$branch}__{$name}.html", $head.$data.'</body></html>');
-}
-
-function glob_recursive($pattern, $flags = 0)
-{
-    $files = glob($pattern, $flags);
-    foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
-        $files = array_merge($files, glob_recursive($dir.'/'.basename($pattern), $flags));
+        file_put_contents("{$outdir}/{$branch}__{$name}.html", $head . $data . '</body></html>');
     }
-    return $files;
-}
+
+    function glob_recursive($pattern, $flags = 0)
+    {
+        $files = glob($pattern, $flags);
+        foreach (glob(dirname($pattern) . '/*', GLOB_ONLYDIR | GLOB_NOSORT) as $dir) {
+            $files = array_merge($files, glob_recursive($dir . '/' . basename($pattern), $flags));
+        }
+        return $files;
+    }
