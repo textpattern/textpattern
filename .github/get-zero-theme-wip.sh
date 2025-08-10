9c1d705a428bbd0c5387b3b8cb2d8c539efd480b
Pete Cooper <petecooper@users.noreply.github.com>
2025-08-10 19:05:44 +0100

    Create get-zero-theme-wip.sh

---
+++ .github/get-zero-theme-wip.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+URL="https://codeload.github.com";
+AUTHOR="textpattern";
+REPO="textpattern-zero-theme";
+EXTRACT="textpattern/setup/themes";
+
+
+TAG="main";
+if [ ! -z "$1" ]; then
+    TAG="$1";
+fi
+
+echo "Get repo: $REPO :: $TAG";
+echo "-------------------------------------------------------------------------------";
+rm -rf $EXTRACT/zero;
+curl $URL/$AUTHOR/$REPO/tar.gz/$TAG | tar xz --strip=2 --directory=$EXTRACT $REPO-$TAG/dist
