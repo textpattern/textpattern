42250a551ba372fc5f7d2fddd14f705f8d82b36a
etc <haut.buis@aliceadsl.fr>
2026-01-14 22:19:51 +0100

    Update taghandlers.php
    see #2033

--- textpattern/publish/taghandlers.php
+++ textpattern/publish/taghandlers.php
@@ -1697,7 +1697,7 @@ function txp_sandbox($atts = array(), $thing = null)
 
     if (empty($id)) {
         assert_article();
-        $id = $thisarticle['thisid'];
+        $id = $thisarticle['thisid'] ?: 0;
     } elseif (!isset($articles[$id])) {
         return;
     }
@@ -1725,7 +1725,7 @@ function txp_sandbox($atts = array(), $thing = null)
     $oldarticle = $thisarticle;
     isset($articles[$id]) and $thisarticle = $articles[$id];
     $was_article_body = $is_article_body;
-    $is_article_body = $thisarticle['authorid'];
+    $is_article_body = $thisarticle['authorid'] ?: true;
     $was_form = $is_form;
     $is_form = 0;
 
