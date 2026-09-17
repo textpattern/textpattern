51bd7054c594dfc2bddf13dd7ec818f8c58ea704
Stef Dawson <thatbloke@stefdawson.com>
2026-09-17 15:05:14 +0100

    Use a static (per login session) CSRF token. Security benefits of unique per-page token are questionable and may break plugins.
    Paritally reverts efbf709.

--- textpattern/include/txp_article.php
+++ textpattern/include/txp_article.php
@@ -415,7 +415,7 @@ function article_preview($field = false)
         $id = intval(ps('ID'));
         $data = array_map('strval', array('id' => $id) + $rs) + array('field' => $field);//$_POST!!!
         ksort($data);
-        $data['id'] = $id . '.' . $token->csrf($txp_user, '') . $token->csrf(json_encode($data), '');
+        $data['id'] = $id . '.' . $token->csrf($txp_user) . $token->csrf(json_encode($data));
         $opts = array(
             'method' => "POST",
             'header' => [
@@ -1822,7 +1822,7 @@ function article_partial_article_view($rs)
     if ($live) {
         $url = permlinkurl_id($rs['ID']);
     } elseif (has_privs('article.preview')) {
-        $url = $ID ? hu . '?id=' . $ID . '.' . urlencode(Txp::get('\Textpattern\Security\Token')->csrf($txp_user, '')) : false; // Article ID plus token.
+        $url = $ID ? hu . '?id=' . $ID . '.' . urlencode(Txp::get('\Textpattern\Security\Token')->csrf($txp_user)) : false; // Article ID plus token.
     } else {
         return;
     }
